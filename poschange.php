60c92fe3a74128de93e1bbd6807dd32273cfe660
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-02-20 22:38:43 -0800

    Rename variables

--- poschange.php
+++ poschange.php
@@ -6,37 +6,37 @@ mysql_connect($dbhost,$dbuname,$dbpass);
 
 echo "<HTML><HEAD><TITLE>Position Change Result</TITLE></HEAD><BODY>";
 
-$Team_Name = $_POST['teamname'];
-$Player_Name = $_POST['playername'];
-$Player_Pos = $_POST['playerpos'];
-$Pos = $_POST['pos'];
+$teamName = $_POST['teamname'];
+$playerName = $_POST['playername'];
+$playerOldPosition = $_POST['playerpos'];
+$playerNewPosition = $_POST['pos'];
 
 $acceptablePositions = array('PG', 'G', 'SG', 'GF', 'SF', 'F', 'PF', 'FC', 'C');
 
-if (in_array($Pos, $acceptablePositions)) {
-    echo "Message from the commissioner's office:<br><font color=#0000cc>Your position change has been submitted and approved. $Player_Name is now a $Pos.</font></br>";
+if (in_array($playerNewPosition, $acceptablePositions)) {
+    echo "Message from the commissioner's office:<br><font color=#0000cc>Your position change has been submitted and approved. $playerName is now a $playerNewPosition.</font></br>";
     echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
 
     $timestamp=date('Y-m-d H:i:s',time());
 
-    $querytopic="SELECT * FROM nuke_topics WHERE topicname = '$Team_Name'";
+    $querytopic="SELECT * FROM nuke_topics WHERE topicname = '$teamName'";
     $resulttopic=mysql_query($querytopic);
     $topicid=mysql_result($resulttopic,0,"topicid");
 
     $querycat="SELECT * FROM nuke_stories_cat WHERE title = 'Position Changes'";
     $resultcat=mysql_query($querycat);
-    $PositionChanges=mysql_result($resultcat,0,"counter");
+    $positionChanges=mysql_result($resultcat,0,"counter");
     $catid=mysql_result($resultcat,0,"catid");
 
-    $PositionChanges=$PositionChanges+1;
+    $positionChanges=$positionChanges+1;
 
     // ==== PUT ANNOUNCEMENT INTO DATABASE ON NEWS PAGE
 
-    $storytitle=$Player_Name." changes his position with the ".$Team_Name;
+    $storytitle=$playerName." changes his position with the ".$teamName;
 
-    $hometext=$Player_Name." today changed his position with the ".$Team_Name." from " .$Player_Pos. " to " .$Pos. ".";
+    $hometext=$playerName." today changed his position with the ".$teamName." from " .$playerOldPosition. " to " .$playerNewPosition. ".";
 
-    $querycat2="UPDATE nuke_stories_cat SET counter = $PositionChanges WHERE title = 'Position Changes'";
+    $querycat2="UPDATE nuke_stories_cat SET counter = $positionChanges WHERE title = 'Position Changes'";
     $resultcat2=mysql_query($querycat2);
 
     $querystor="INSERT INTO nuke_stories (catid,aid,title,time,hometext,topic,informant,counter,alanguage) VALUES ('$catid','Associated Press','$storytitle','$timestamp','$hometext','$topicid','Associated Press','0','english')";
@@ -44,13 +44,13 @@ if (in_array($Pos, $acceptablePositions)) {
 
     // ==== UPDATE POSITION CHANGES USED AND ALT POSITION IN DATABASE ====
 
-    $queryseason="UPDATE nuke_ibl_team_info SET poschanges = poschanges + 1 WHERE team_name = '$Team_Name'";
+    $queryseason="UPDATE nuke_ibl_team_info SET poschanges = poschanges + 1 WHERE team_name = '$teamName'";
     $resultseason=mysql_query($queryseason);
 
-    $queryseason1="UPDATE nuke_iblplyr SET altpos = '$Pos' WHERE name = '$Player_Name'";
+    $queryseason1="UPDATE nuke_iblplyr SET altpos = '$playerNewPosition' WHERE name = '$playerName'";
     $resultseason1=mysql_query($queryseason1);
 
-    $queryseason2="UPDATE nuke_iblplyr SET poschange = '1' WHERE name = '$Player_Name'";
+    $queryseason2="UPDATE nuke_iblplyr SET poschange = '1' WHERE name = '$playerName'";
     $resultseason2=mysql_query($queryseason2);
 } else {
     echo "Message from the commissioner's office: <font color=#FF0000>Your position change has been DECLINED: that was not a valid position.</font><p>";

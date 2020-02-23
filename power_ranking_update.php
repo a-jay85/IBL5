e8f2621acba3c6e792f85dd48ebc618407053e8a
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-02-22 22:06:39 -0800

    more lowercase ibl_schedule capitalization

--- power_ranking_update.php
+++ power_ranking_update.php
@@ -129,7 +129,7 @@ SET iblhoops_iblv2forums.forum_stats.ast_pid = (SELECT pid FROM iblhoops_ibl5.nu
 
 function record($tid)
 {
-	$query = "SELECT * FROM IBL_Schedule WHERE (Visitor = $tid OR Home = $tid) AND BoxID > 0 ORDER BY Date ASC";
+	$query = "SELECT * FROM ibl_schedule WHERE (Visitor = $tid OR Home = $tid) AND BoxID > 0 ORDER BY Date ASC";
 	$result = mysql_query($query);
 	$num = mysql_numrows($result);
 	$wins = 0;
@@ -209,7 +209,7 @@ function last($tid)
 
 function ranking($tid, $wins, $losses)
 {
-	$query = "SELECT * FROM IBL_Schedule WHERE Visitor = $tid AND BoxID > 0 ORDER BY Date ASC";
+	$query = "SELECT * FROM ibl_schedule WHERE Visitor = $tid AND BoxID > 0 ORDER BY Date ASC";
 	$result = mysql_query($query);
 	$num = mysql_numrows($result);
 	$winpoints = 0;
@@ -237,7 +237,7 @@ function ranking($tid, $wins, $losses)
 		$i++;
 	}
 
-	$query = "SELECT * FROM IBL_Schedule WHERE Home = $tid AND BoxID > 0 ORDER BY Date ASC";
+	$query = "SELECT * FROM ibl_schedule WHERE Home = $tid AND BoxID > 0 ORDER BY Date ASC";
 	$result = mysql_query($query);
 	$num = mysql_numrows($result);
 	$i = 0;

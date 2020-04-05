d4590dacf51138717f6fd5ad071f06c5f095d700
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-04-04 17:03:51 -0700

    initial commit

---
+++ playoffStatLeaders.php
@@ -0,0 +1,152 @@
+<?php
+
+require 'config.php';
+mysql_connect($dbhost,$dbuname,$dbpass);
+@mysql_select_db($dbname) or die("Unable to select database");
+
+$arrayStatNames = array(
+    'POINTS',
+    'REBOUNDS',
+    'ASSISTS',
+    'STEALS',
+    'BLOCKS',
+    'TURNOVERS',
+    'Field Goals Made',
+    'Free Throws Made',
+    'Three Pointers Made'
+);
+
+$arrayStatQueries = array(
+    '(`game2GM`*2) + `gameFTM` + (`game3GM`*3)',
+    '(`gameORB` + `gameDRB`)',
+    '`gameAST`',
+    '`gameSTL`',
+    '`gameBLK`',
+    '`gameTOV`',
+    '(`game2GM` + `game3GM`)',
+    '`gameFTM`',
+    '`game3GM`'
+);
+
+function seasonHighTable($queryForStat, $statName, $playerOrTeam)
+{
+    if ($playerOrTeam == 'player') {
+        $isPlayer = 'pid != 0';
+    } elseif ($playerOrTeam == 'team') {
+        $isPlayer = 'pid = 0';
+    }
+
+    $query = "SELECT `name`, `date`, " . $queryForStat . " AS `" . $statName . "`
+        FROM ibl_box_scores
+        WHERE " . $isPlayer . "
+        ORDER BY `" . $statName . "` DESC, date ASC LIMIT 15;";
+    $result = mysql_query($query);
+    $numRows = mysql_num_rows($result);
+
+    echo "\t\t\t<table border=1>\n";
+    echo "\t\t\t\t<th colspan=4 align=center>$statName</th>\n";
+    $i = 0;
+    while ($i < $numRows) {
+        echo "\t\t\t\t<tr>\n";
+        echo "\t\t\t\t\t<td align=center>\n";
+        echo "\t\t\t\t\t\t" . ($i + 1) . "\n";
+        echo "\t\t\t\t\t</td>\n";
+        $j = 0;
+        while ($j < 3) {
+            echo "\t\t\t\t\t<td>\n";
+            echo "\t\t\t\t\t\t" . mysql_result($result, $i, $j) . "\n";
+            echo "\t\t\t\t\t</td>\n";
+            $j++;
+        }
+        echo "\t\t\t\t</tr>\n";
+        $i++;
+    }
+    echo "\t\t\t</table>\n";
+}
+
+function nextTableColumn ()
+{
+    echo "\t\t</td>\n";
+    echo "\t\t<td align=center>\n";
+}
+
+function startTableRow ()
+{
+    echo "\t<tr>\n";
+    echo "\t\t<td align=center>\n";
+}
+
+function endTableRow ()
+{
+    echo "\t\t</td>\n";
+    echo "\t</tr>\n";
+}
+
+
+echo "<html><head><title>Season Stat Leaders</title></head>\n\n";
+echo "<body>\n\n";
+
+echo "<H1>Players' Season Highs<H1>\n\n";
+$playerOrTeam = 'player';
+
+echo "<table cellpadding=5>\n";
+
+startTableRow();
+seasonHighTable(reset($arrayStatQueries), reset($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+startTableRow();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+startTableRow();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+echo "</table>\n\n";
+
+echo "<H1>Teams' Season Highs</H1>\n\n";
+$playerOrTeam = 'team';
+
+echo "<table cellpadding=5>\n";
+
+startTableRow();
+seasonHighTable(reset($arrayStatQueries), reset($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+startTableRow();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+startTableRow();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+nextTableColumn();
+seasonHighTable(next($arrayStatQueries), next($arrayStatNames), $playerOrTeam);
+endTableRow();
+
+echo "</table>\n\n";
+
+echo "</body>\n";
+echo "</html>";

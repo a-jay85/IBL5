d817b6bac9ba9426a2840884b4abcbe705ebebf7
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-06-21 15:06:03 -0700

    formatting

--- ibl5/blocks/block-Injury.php
+++ ibl5/blocks/block-Injury.php
@@ -11,7 +11,7 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
-if ( !defined('BLOCK_FILE') ) {
+if (!defined('BLOCK_FILE')) {
     Header("Location: ../index.php");
     die();
 }
@@ -22,54 +22,70 @@ require 'config.php';
 mysql_connect($dbhost,$dbuname,$dbpass);
 @mysql_select_db($dbname) or die("Unable to select database");
 
-$queryo="SELECT * FROM nuke_users WHERE user_ibl_team != '' ORDER BY user_ibl_team ASC";
-$resulto=mysql_query($queryo);
-$numo=mysql_numrows($resulto);
+$queryo = "SELECT * FROM nuke_users WHERE user_ibl_team != '' ORDER BY user_ibl_team ASC";
+$resulto = mysql_query($queryo);
+$numo = mysql_numrows($resulto);
 
-$content = "<table border=0><tr><td colspan=4><b>The following teams need to submit a new lineup or sign player(s) from waivers due to injury:</b></td></tr><tr><td bgcolor=#000066><font color=#ffffff><b>TEAM NAME</td><td bgcolor=#000066><font color=#ffffff><b>HEALTHY PLAYERS</td><td bgcolor=#000066><font color=#ffffff><b>WAIVERS NEEDED</td><td bgcolor=#000066><font color=#ffffff><b>NEW LINEUP NEEDED</td></tr>";
+$content = "<table border=0>
+    <tr>
+        <td colspan=4>
+            <b>The following teams need to submit a new lineup or sign player(s) from waivers due to injury:</b>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor=#000066>
+            <font color=#ffffff><b>TEAM NAME</b></font>
+        </td>
+        <td bgcolor=#000066>
+            <font color=#ffffff><b>HEALTHY PLAYERS</b></font>
+        </td>
+        <td bgcolor=#000066>
+            <font color=#ffffff><b>WAIVERS NEEDED</b></font>
+        </td>
+        <td bgcolor=#000066>
+            <font color=#ffffff><b>NEW LINEUP NEEDED</b></font>
+        </td>
+    </tr>";
 
-$j=0;
-while ($j < $numo)
-{
-	$user_team=mysql_result($resulto,$j,"user_ibl_team");
+$j = 0;
+while ($j < $numo) {
+	$user_team = mysql_result($resulto, $j, "user_ibl_team");
 
-	$sql="SELECT * FROM nuke_iblplyr WHERE teamname='$user_team' AND retired = '0' AND injured = '0' and droptime = '0' and name NOT LIKE '%Buyout%' ORDER BY ordinal ASC ";
+	$sql = "SELECT * FROM nuke_iblplyr WHERE teamname = '$user_team' AND retired = '0' AND injured = '0' and droptime = '0' and name NOT LIKE '%Buyout%' ORDER BY ordinal ASC ";
 	$result1 = mysql_query($sql);
-	$num1=mysql_numrows($result1);
+	$num1 = mysql_numrows($result1);
 
-	$sql2="SELECT * FROM nuke_iblplyr WHERE teamname='$user_team' AND retired = '0' AND injured > '0' AND active = '1' ORDER BY ordinal ASC ";
+	$sql2 = "SELECT * FROM nuke_iblplyr WHERE teamname='$user_team' AND retired = '0' AND injured > '0' AND active = '1' ORDER BY ordinal ASC ";
 	$result2 = mysql_query($sql2);
-	$num2=mysql_numrows($result2);
+	$num2 = mysql_numrows($result2);
 
-	if ($num2>0)
-	{
-		$new_lineups='Yes';
-	}else{
-		$new_lineups='No';
+	if ($num2 > 0) {
+		$new_lineups = 'Yes';
+	} else {
+		$new_lineups = 'No';
 	}
 
-	$waivers_needed=12;
-	$healthy=0;
-	$i=0;
-	while($i < $num1)
-	{
+	$waivers_needed = 12;
+	$healthy = 0;
+	$i = 0;
+	while ($i < $num1) {
 		$healthy++;
 		$i++;
 	}
 
 	$waivers_needed = $waivers_needed - $healthy;
-	if ($waivers_needed < 0)
-	{
-		$waivers_needed=0;
+	if ($waivers_needed < 0) {
+		$waivers_needed = 0;
 	}
-	$sql3="SELECT chart FROM nuke_ibl_team_info WHERE team_name='$user_team'";
+	$sql3 = "SELECT chart FROM nuke_ibl_team_info WHERE team_name = '$user_team'";
 	$result3 = mysql_query($sql3);
-	$chart = mysql_result($result3,0,"chart");
+	$chart = mysql_result($result3, 0, "chart");
 
-	if ($waivers_needed>0 || $new_lineups=='Yes' && $chart == 0)
-	{
+	if ($waivers_needed > 0 || $new_lineups == 'Yes' && $chart == 0) {
 		$content .= "<tr><td>$user_team</td><td>$healthy</td><td>$waivers_needed</td><td>$new_lineups</td></tr>";
-	}	$j++;
+	}
+
+    $j++;
 }
 
 $content .= "</table>";

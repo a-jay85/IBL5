9262ec651666dc5ec980c08c42868957ac4ac786
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-10 19:50:52 -0700

    Sync sharedFunctions.php with ibl5/sharedFunctions.php

--- sharedFunctions.php
+++ sharedFunctions.php
@@ -9,3 +9,23 @@ function getLastSimDatesArray()
 
     return mysql_fetch_assoc($queryLastSimDates);
 }
+
+function getCurrentSeasonEndingYear()
+{
+    $queryCurrentSeasonEndingYear = mysql_query("SELECT value
+        FROM nuke_ibl_settings
+        WHERE name = 'Current Season Ending Year'
+        LIMIT 1");
+
+    return mysql_result($queryCurrentSeasonEndingYear, 0);
+}
+
+function getCurrentSeasonPhase()
+{
+    $queryCurrentSeasonPhase = mysql_query("SELECT value
+        FROM nuke_ibl_settings
+        WHERE name = 'Current Season Phase'
+        LIMIT 1");
+
+    return mysql_result($queryCurrentSeasonPhase, 0);
+}

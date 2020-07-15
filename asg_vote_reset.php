488d76f58e6004788c6b078eb44e480f672ee02b
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-15 00:45:25 -0700

    replace voting modules' 'sid' references with 'name' references

--- asg_vote_reset.php
+++ asg_vote_reset.php
@@ -11,7 +11,7 @@ mysql_connect(localhost,$username,$password);
 $query1="UPDATE nuke_asg_votes SET East_C = NULL, East_F1 = NULL, East_F2 = NULL, East_G1 = NULL, East_G2 = NULL, West_C = NULL, West_F1 = NULL, West_F2 = NULL, West_G1 = NULL, West_G2 = NULL";
 $result1=mysql_query($query1);
 
-$query2="UPDATE nuke_ibl_settings SET value = 'Yes' where sid = 23";
+$query2="UPDATE nuke_ibl_settings SET value = 'Yes' where name = 'ASG Voting'";
 $result2=mysql_query($query2);
 
 $query3="UPDATE ibl_team_history SET asg_vote = 'No Vote'";

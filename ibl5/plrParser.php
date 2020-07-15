899e519adee62103a77e205cda26f367f074a231
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-14 21:00:32 -0700

    clarify variable names

--- ibl5/plrParser.php
+++ ibl5/plrParser.php
@@ -23,21 +23,21 @@ while (!feof($plrFile)) {
     $tid = substr($line,44,2);
     $peak = substr($line,46,4);
     $pos = substr($line,50,2);
-    $rlGP = substr($line,52,4);
-    $rlMIN = substr($line,56,4);
-    $rlFGM = substr($line,60,4);
-    $rlFGA = substr($line,64,4);
-    $rlFTM = substr($line,68,4);
-    $rlFTA = substr($line,72,4);
-    $rl3GM = substr($line,76,4);
-    $rl3GA = substr($line,80,4);
-    $rlORB = substr($line,84,4);
-    $rlDRB = substr($line,88,4);
-    $rlAST = substr($line,92,4);
-    $rlSTL = substr($line,96,4);
-    $rlTVR = substr($line,100,4);
-    $rlBLK = substr($line,104,4);
-    $rlPF = substr($line,108,4);
+    $realLifeGP = substr($line,52,4);
+    $realLifeMIN = substr($line,56,4);
+    $realLifeFGM = substr($line,60,4);
+    $realLifeFGA = substr($line,64,4);
+    $realLifeFTM = substr($line,68,4);
+    $realLifeFTA = substr($line,72,4);
+    $realLife3GM = substr($line,76,4);
+    $realLife3GA = substr($line,80,4);
+    $realLifeORB = substr($line,84,4);
+    $realLifeDRB = substr($line,88,4);
+    $realLifeAST = substr($line,92,4);
+    $realLifeSTL = substr($line,96,4);
+    $realLifeTVR = substr($line,100,4);
+    $realLifeBLK = substr($line,104,4);
+    $realLifePF = substr($line,108,4);
     $oo = substr($line,112,2);
     $od = substr($line,114,2);
     $do = substr($line,116,2);
@@ -55,9 +55,9 @@ while (!feof($plrFile)) {
     $CDepth = substr($line,136,1);
     $active = substr($line,137,1);
     // 138,2 = ?
-    $injured = substr($line,140,4);
-    $seasonGS = substr($line,144,4);
-    $seasonGP = substr($line,148,4);
+    $injuryDaysLeft = substr($line,140,4);
+    $seasonGamesStarted = substr($line,144,4);
+    $seasonGamesPlayed = substr($line,148,4);
     $seasonMIN = substr($line,152,4);
     $season2GM = substr($line,156,4);
     $season2GA = substr($line,160,4);
@@ -109,7 +109,7 @@ while (!feof($plrFile)) {
     $contractYear6 = substr($line,318,4);
     // 322,4 = ?
     $draftRound = substr($line,326,2);
-    $draftPickNum = substr($line,328,2);
+    $draftPickNumber = substr($line,328,2);
     // 330-331 = ?
     $contractOwnedBy = substr($line,332,2);
     // 334-340 = ?
@@ -118,8 +118,8 @@ while (!feof($plrFile)) {
     $seasonHighAST = substr($line,345,2);
     $seasonHighSTL = substr($line,347,2);
     $seasonHighBLK = substr($line,349,2);
-    $seasonHighDD = substr($line,351,2);
-    $seasonHighTD = substr($line,353,2);
+    $seasonHighDoubleDoubles = substr($line,351,2);
+    $seasonHighTripleDoubles = substr($line,353,2);
     $seasonPlayoffHighPTS = substr($line,355,2);
     $seasonPlayoffHighREB = substr($line,357,2);
     $seasonPlayoffHighAST = substr($line,359,2);
@@ -130,8 +130,8 @@ while (!feof($plrFile)) {
     $careerSeasonHighAST = substr($line,377,6);
     $careerSeasonHighSTL = substr($line,383,6);
     $careerSeasonHighBLK = substr($line,389,6);
-    $careerSeasonHighDD = substr($line,395,6);
-    $careerSeasonHighTD = substr($line,401,6);
+    $careerSeasonHighDoubleDoubles = substr($line,395,6);
+    $careerSeasonHighTripleDoubles = substr($line,401,6);
     $careerPlayoffHighPTS = substr($line,407,6);
     $careerPlayoffHighREB = substr($line,413,6);
     $careerPlayoffHighAST = substr($line,419,6);
@@ -188,10 +188,10 @@ while (!feof($plrFile)) {
     $heightFT = floor($heightInches / 12);
     $heightIN = $heightInches % 12;
     $draftYear = $currentSeasonEndingYear - $exp;
-    if ($rlPF != 0) {
-        $minsPerPF = round($rlMIN / $rlPF);
+    if ($realLifePF != 0) {
+        $minutesPerPF = round($realLifeMIN / $realLifePF);
     } else {
-        $minsPerPF = 0;
+        $minutesPerPF = 0;
     }
 
     if ($ordinal <= 1440) {
@@ -341,8 +341,8 @@ while (!feof($plrFile)) {
             $PFDepth,
             $CDepth,
             $active,
-            $seasonGS,
-            $seasonGP,
+            $seasonGamesStarted,
+            $seasonGamesPlayed,
             $seasonMIN,
             $seasonFGM,
             $seasonFGA,
@@ -381,8 +381,8 @@ while (!feof($plrFile)) {
             $seasonHighAST,
             $seasonHighSTL,
             $seasonHighBLK,
-            $seasonHighDD,
-            $seasonHighTD,
+            $seasonHighDoubleDoubles,
+            $seasonHighTripleDoubles,
             $seasonPlayoffHighPTS,
             $seasonPlayoffHighREB,
             $seasonPlayoffHighAST,
@@ -393,8 +393,8 @@ while (!feof($plrFile)) {
             $careerSeasonHighAST,
             $careerSeasonHighSTL,
             $careerSeasonHighBLK,
-            $careerSeasonHighDD,
-            $careerSeasonHighTD,
+            $careerSeasonHighDoubleDoubles,
+            $careerSeasonHighTripleDoubles,
             $careerPlayoffHighPTS,
             $careerPlayoffHighREB,
             $careerPlayoffHighAST,
@@ -430,14 +430,14 @@ while (!feof($plrFile)) {
             $ratingTVR,
             $ratingBLK,
             $draftRound,
-            $draftPickNum,
-            $injured,
+            $draftPickNumber,
+            $injuryDaysLeft,
             $heightFT,
             $heightIN,
             $weight,
             $draftYear,
             0,
-            $minsPerPF
+            $minutesPerPF
         )
         ON DUPLICATE KEY UPDATE
             `ordinal` = $ordinal,
@@ -463,8 +463,8 @@ while (!feof($plrFile)) {
             `PFDepth` = $PFDepth,
             `CDepth` = $CDepth,
             `active` = $active,
-            `stats_gs` = $seasonGS,
-            `stats_gm` = $seasonGP,
+            `stats_gs` = $seasonGamesStarted,
+            `stats_gm` = $seasonGamesPlayed,
             `stats_min` = $seasonMIN,
             `stats_fgm` = $seasonFGM,
             `stats_fga` = $seasonFGA,
@@ -503,8 +503,8 @@ while (!feof($plrFile)) {
             `sh_ast` = $seasonHighAST,
             `sh_stl` = $seasonHighSTL,
             `sh_blk` = $seasonHighBLK,
-            `s_dd` = $seasonHighDD,
-            `s_td` = $seasonHighTD,
+            `s_dd` = $seasonHighDoubleDoubles,
+            `s_td` = $seasonHighTripleDoubles,
             `sp_pts` = $seasonPlayoffHighPTS,
             `sp_reb` = $seasonPlayoffHighREB,
             `sp_ast` = $seasonPlayoffHighAST,
@@ -515,8 +515,8 @@ while (!feof($plrFile)) {
             `ch_ast` = $careerSeasonHighAST,
             `ch_stl` = $careerSeasonHighSTL,
             `ch_blk` = $careerSeasonHighBLK,
-            `c_dd` = $careerSeasonHighDD,
-            `c_td` = $careerSeasonHighTD,
+            `c_dd` = $careerSeasonHighDoubleDoubles,
+            `c_td` = $careerSeasonHighTripleDoubles,
             `cp_pts` = $careerPlayoffHighPTS,
             `cp_reb` = $careerPlayoffHighREB,
             `cp_ast` = $careerPlayoffHighAST,
@@ -552,14 +552,14 @@ while (!feof($plrFile)) {
             `r_to` = $ratingTVR,
             `r_blk` = $ratingBLK,
             `draftround` = $draftRound,
-            `draftpickno` = $draftPickNum,
-            `injured` = $injured,
+            `draftpickno` = $draftPickNumber,
+            `injured` = $injuryDaysLeft,
             `htft` = $heightFT,
             `htin` = $heightIN,
             `wt` = $weight,
             `draftyear` = $draftYear,
             `retired` = 0,
-            `r_foul` = $minsPerPF;";
+            `r_foul` = $minutesPerPF;";
         if ($pid != 0) {
             if (mysql_query($playerUpdateQuery)) {
                 echo $pid . '<p>';
@@ -576,8 +576,8 @@ while (!feof($plrFile)) {
 
         $teamUpdateQuery = 'UPDATE `ibl_team_'.$sideOfTheBall.'_stats__test`
             SET
-            `games` = '.$seasonGP.',
-            `minutes` = '.($seasonGP*48).',
+            `games` = '.$seasonGamesPlayed.',
+            `minutes` = '.($seasonGamesPlayed*48).',
             `fgm` = '.($season2GM+$season3GM).',
             `fga` = '.($season2GA+$season3GA).',
             `ftm` = '.$seasonFTM.',

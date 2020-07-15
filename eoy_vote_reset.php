fd78c17edaa58316774536803d86c07c4129bbc0
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-15 00:45:25 -0700

    replace voting modules' 'sid' references with 'name' references

--- eoy_vote_reset.php
+++ eoy_vote_reset.php
@@ -11,7 +11,7 @@ mysql_connect(localhost,$username,$password);
 $query1="UPDATE nuke_eoy_votes SET MVP_1 = NULL, MVP_2 = NULL, MVP_3 = NULL, Six_1 = NULL, Six_2 = NULL, Six_3 = NULL, ROY_1 = NULL, ROY_2 = NULL, ROY_3 = NULL, GM_1 = NULL, GM_2 = NULL, GM_3 = NULL";
 $result1=mysql_query($query1);
 
-$query2="UPDATE nuke_ibl_settings SET value = 'Yes' where sid = 24";
+$query2="UPDATE nuke_ibl_settings SET value = 'Yes' where name = 'EOY Voting'";
 $result2=mysql_query($query2);
 
 $query3="UPDATE ibl_team_history SET eoy_vote = 'No Vote'";

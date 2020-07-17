921e6eb452cf82a426f1303db4d812731497d8e9
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-16 20:42:00 -0700

    Delete second table; make table sortable

--- ibl5/online/TeamRosterChange.php
+++ ibl5/online/TeamRosterChange.php
@@ -11,8 +11,17 @@ $previousSeasonEndingYear = $currentSeasonEndingYear - 1;
 $query="SELECT a.name, a.teamid, a.team, b.tid, b.teamname FROM nuke_iblhist a, nuke_iblplyr b WHERE a.pid = b.pid AND a.year = $previousSeasonEndingYear AND a.teamid != b.tid ORDER BY b.teamname";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
-echo "<table border=1><tr><td>";
-echo "<table border=1><tr><td colspan=3><b>GAINS</td></tr><tr><td><b>Player</td><td><b>New Team</td><td><b>Old Team</td></tr>";
+
+echo "<script src=\"http://www.iblhoops.net/jslib/sorttable.js\"></script>
+<center><h1>OFFSEASON MOVEMENT</h1>
+<i>Click the headings to sort the table</i>
+<table border=1><tr><td>";
+echo "<table border=1 class=\"sortable\">
+	<tr>
+		<th><b>Player</th>
+		<th><b>New Team</th>
+		<th><b>Old Team</th>
+	</tr>";
 
 $i=0;
 
@@ -24,22 +33,5 @@ while ($i < $num)
 	echo "<tr><td>$playername</td><td>$newteam</td><td>$oldteam</td></tr>";
 	$i++;
 }
-echo "</table></td><td width=50></td><td>";
-echo "<table border=1><tr><td colspan=3><b>LOSSES</td></tr><tr><td><b>Player</td><td><b>Old Team</td><td><b>New Team</td></tr>";
-
-
-$query2="SELECT a.name, a.teamid, a.team, b.tid, b.teamname FROM nuke_iblhist a, nuke_iblplyr b WHERE a.pid = b.pid AND a.year = $previousSeasonEndingYear AND a.teamid != b.tid ORDER BY a.team";
-$result2=mysql_query($query2);
-$num2=mysql_numrows($result2);
-$j=0;
-
-while ($j < $num2)
-{
-	$playername2=mysql_result($result2,$j,"a.name");
-	$oldteam2=mysql_result($result2,$j,"a.team");
-	$newteam2=mysql_result($result2,$j,"b.teamname");
-	echo "<tr><td>$playername2</td><td>$oldteam2</td><td>$newteam2</td></tr>";
-	$j++;
-}
-echo "</td></tr></table>";
+echo "</table></center>";
 ?>

7bfd57dcf1790f25b8da8c76be9bd22eea26351b
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-07-21 22:16:18 -0700

    require_once sharedFunctions.php

--- ibl5/leagueControlPanel.php
+++ ibl5/leagueControlPanel.php
@@ -4,7 +4,7 @@ require 'config.php';
 mysql_connect($dbhost,$dbuname,$dbpass);
 @mysql_select_db($dbname) or die("Unable to select database");
 
-require $_SERVER['DOCUMENT_ROOT'] . '/sharedFunctions.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/sharedFunctions.php';
 
 $queryString = "";
 $successText = "";

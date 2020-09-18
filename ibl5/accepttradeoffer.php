5fb711018b6c1cb8090472b79e1822e3f80181d5
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-09-17 23:31:34 -0700

    change syntax of concatenation

--- ibl5/accepttradeoffer.php
+++ ibl5/accepttradeoffer.php
@@ -26,7 +26,7 @@ while ($i < $num0) {
 	if ($itemtype == 0) {
 		$queryj = "SELECT * FROM ibl_draft_picks WHERE `pickid` = '$itemid'";
 		$resultj = mysql_query($queryj);
-		$storytext = $storytext . "The $from send the " . mysql_result($resultj, 0, "year") . " " . mysql_result($resultj, 0, "teampick") . " Round " . mysql_result($resultj, 0, "round") . " draft pick to the $to.<br>";
+		$storytext .= "The $from send the " . mysql_result($resultj, 0, "year") . " " . mysql_result($resultj, 0, "teampick") . " Round " . mysql_result($resultj, 0, "round") . " draft pick to the $to.<br>";
 
 		$queryi = 'UPDATE ibl_draft_picks SET `ownerofpick` = "' . $to . '" WHERE `pickid` = ' . $itemid . ' LIMIT 1;';
 		$resulti = mysql_query($queryi);
@@ -38,7 +38,7 @@ while ($i < $num0) {
 		$queryk = "SELECT * FROM nuke_iblplyr WHERE pid = '$itemid'";
 		$resultk = mysql_query($queryk);
 
-		$storytext = $storytext . "The $from send " . mysql_result($resultk, 0, "pos") . " " . mysql_result($resultk, 0, "name") . " to the $to.<br>";
+		$storytext .= "The $from send " . mysql_result($resultk, 0, "pos") . " " . mysql_result($resultk, 0, "name") . " to the $to.<br>";
 
 		$queryi = 'UPDATE nuke_iblplyr SET `teamname` = "' . $to . '", `tid` = ' . $tid . ' WHERE `pid` = ' . $itemid . ' LIMIT 1;';
 		$resulti = mysql_query($queryi);

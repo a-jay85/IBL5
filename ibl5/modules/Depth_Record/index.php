5d730d8b38cce2ffb3909747ced61b3407521fe7
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-09-23 20:27:20 -0700

    formatting

--- ibl5/modules/Depth_Record/index.php
+++ ibl5/modules/Depth_Record/index.php
@@ -22,31 +22,39 @@ get_lang($module_name);
 $userpage = 1;
 include("header.php");
 
-$query2="SELECT * FROM ibl_team_history WHERE teamid != 35 ORDER BY teamid ASC"; // Grab all teams except for the Free Agents
-$result2=mysql_query($query2);
-$num2=mysql_num_rows($result2);
+$query2 = "SELECT * FROM ibl_team_history WHERE teamid != 35 ORDER BY teamid ASC"; // Grab all teams except for the Free Agents
+$result2 = mysql_query($query2);
+$num2 = mysql_num_rows($result2);
 
 OpenTable();
 
-$k=0;
-while ($k < $num2)
-{
-	$teamname[$k]=mysql_result($result2,$k,"team_name");
-	$teamcity[$k]=mysql_result($result2,$k,"team_city");
-	$teamcolor1[$k]=mysql_result($result2,$k,"color1");
-	$teamcolor2[$k]=mysql_result($result2,$k,"color2");
-        $depth[$k]=mysql_result($result2,$k,"depth");
-        $simdepth[$k]=mysql_result($result2,$k,"sim_depth");
-	$teamid[$k]=mysql_result($result2,$k,"teamid");
+$k = 0;
+while ($k < $num2) {
+	$teamname[$k] = mysql_result($result2, $k, "team_name");
+	$teamcity[$k] = mysql_result($result2, $k, "team_city");
+	$teamcolor1[$k] = mysql_result($result2, $k, "color1");
+	$teamcolor2[$k] = mysql_result($result2, $k, "color2");
+    $depth[$k] = mysql_result($result2, $k, "depth");
+    $simdepth[$k] = mysql_result($result2, $k, "sim_depth");
+	$teamid[$k] = mysql_result($result2, $k, "teamid");
 
-
-	$table_echo=$table_echo."<tr><td bgcolor=#".$teamcolor1[$k]."><a href=\"modules.php?name=Team&op=team&tid=".$teamid[$k]."\"><font color=#".$teamcolor2[$k].">".$teamcity[$k]." ".$teamname[$k]."</a></td><td>".$simdepth[$k]."</td><td>".$depth[$k]."</td></tr>";
+	$table_echo .= "<tr>
+		<td bgcolor=#" . $teamcolor1[$k] . "><a href=\"modules.php?name=Team&op=team&tid=" . $teamid[$k] . "\"><font color=#" . $teamcolor2[$k] . ">" . $teamcity[$k] . " " . $teamname[$k] . "</a></td>
+		<td>" . $simdepth[$k] . "</td>
+		<td>" . $depth[$k] . "</td>
+	</tr>";
 
 	$k++;
 }
 
-$text=$text."<table class=\"sortable\" border=1>
-		  <tr><th>Team</th><th>Sim Depth Chart Received</th><th>Last Depth Chart Received</th></tr>$table_echo</table>";
+$text .= "<table class=\"sortable\" border=1>
+	<tr>
+	  	<th>Team</th>
+		<th>Sim Depth Chart Received</th>
+		<th>Last Depth Chart Received</th>
+	</tr>$table_echo
+</table>";
+
 echo $text;
 
 CloseTable();

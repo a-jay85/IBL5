3580de1ddd89842eacae225b1c15282509a1bf27
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-09-27 14:44:28 -0700

    Add "Set Free Agency factors for PFW" button; add better error support

--- ibl5/leagueControlPanel.php
+++ ibl5/leagueControlPanel.php
@@ -10,6 +10,7 @@ $queryString = "";
 $successText = "";
 
 $currentSeasonPhase = getCurrentSeasonPhase();
+$currentSeasonEndingYear = getCurrentSeasonEndingYear();
 
 if (isset($_POST['query'])) {
     switch ($_POST['query']) {
@@ -34,6 +35,18 @@ if (isset($_POST['query'])) {
         case 'Set all players on waivers to Free Agents and reset their Bird years':
             $queryString = "UPDATE nuke_iblplyr SET teamname = 'Free Agents', bird = 0 WHERE retired != 1 AND ordinal >= 960;";
             $successText = "All players currently on waivers have their teamname set to Free Agents and 0 Bird years.";
+            break;
+        case 'Set Free Agency factors for PFW':
+            if ($currentSeasonPhase == 'Draft' OR $currentSeasonPhase == 'Free Agency') {
+                $queryString = "UPDATE nuke_ibl_team_info info, nuke_ibl_power power
+                    SET Contract_Wins = power.win,
+                    	Contract_Losses = power.loss
+                    WHERE power.TeamID = info.teamid;";
+                $successText = "The columns that affect each team's Play For Winner demand factor have been updated to match this past season's ($currentSeasonEndingYear) win/loss records.";
+            } else {
+                $failureText = "Sorry, that button can only be used during the Draft or Free Agency.<br>
+                    The FA demands formula requires the current season to be finished before calculating factors.";
+            }
             break;
     }
 
@@ -65,14 +78,17 @@ echo "<FORM action=\"leagueControlPanel.php\" method=\"POST\">
     <INPUT type='submit' name='query' value='Reset All MLEs/LLEs'><p>
     <INPUT type='submit' name='query' value='Set all undefined player positions'><p>
     <INPUT type='submit' name='query' value='Set all players on waivers to Free Agents and reset their Bird years'><p>
+    <INPUT type='submit' name='query' value='Set Free Agency factors for PFW'><p>
 </FORM>\n";
 
 if ($querySuccessful == TRUE) {
     echo "<code>" . $queryString . "</code>";
     echo "<p>";
     echo "<b>" . $successText . "</b>";
-    echo "Oops, something went wrong. Let A-Jay know what you were trying to do and he'll look into it.";
 } else {
+    echo "Oops, something went wrong:<p>
+    <FONT color=red>$failureText</FONT><p>
+    Let A-Jay know what you were trying to do and he'll look into it.";
 };
 
 echo "

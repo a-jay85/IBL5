899516c8d3956f9b6bdb03f793e854843eddbaaa
A-Jay Nicolas <ajaynicolas@gmail.com>
2020-12-10 18:02:47 -0800

    Add "Set Allow Trades Status" to Regular Season leagueControlPanel page

--- ibl5/leagueControlPanel.php
+++ ibl5/leagueControlPanel.php
@@ -12,6 +12,7 @@ $successText = "";
 $currentSeasonPhase = getCurrentSeasonPhase();
 $currentSeasonEndingYear = getCurrentSeasonEndingYear();
 $waiverWireStatus = getWaiverWireStatus();
+$allowTradesStatus = getAllowTradesStatus();
 
 if (isset($_POST['query'])) {
     switch ($_POST['query']) {
@@ -26,6 +27,12 @@ if (isset($_POST['query'])) {
                 $queryString = "UPDATE nuke_ibl_settings SET value = '{$_POST['Waivers']}' WHERE name = 'Allow Waiver Moves';";
             }
             $successText = "Waiver Wire Status has been set to {$_POST['Waivers']}.";
+            break;
+        case 'Set Allow Trades Status':
+            if (isset($_POST['Trades'])) {
+                $queryString = "UPDATE nuke_ibl_settings SET value = '{$_POST['Trades']}' WHERE name = 'Allow Trades';";
+            }
+            $successText = "Allow Trades Status has been set to {$_POST['Trades']}.";
             break;
         case 'Reset All Contract Extensions':
             $queryString = "UPDATE nuke_ibl_team_info SET Used_Extension_This_Season = 0;";
@@ -73,6 +80,9 @@ if (isset($_POST['query'])) {
         if (isset($_POST['Waivers'])) {
             $waiverWireStatus = $_POST['Waivers'];
         }
+        if (isset($_POST['Trades'])) {
+            $allowTradesStatus = $_POST['Trades'];
+        }
     } else {
         $querySuccessful = FALSE;
     };
@@ -117,7 +127,12 @@ switch ($currentSeasonPhase) {
         echo "<A HREF=\"updateAllTheThings.php\">Update All The Things</A><p>
             <A HREF=\"scoParser.php\">Run scoParser.php</A><p>
             <A HREF=\"asg_vote_reset.php\">Reset All-Star Voting</A><p>
-            <A HREF=\"eoy_vote_reset.php\">Reset End of the Year Voting</A><p>";
+            <A HREF=\"eoy_vote_reset.php\">Reset End of the Year Voting</A><p>
+            <select name=\"Trades\">
+                <option value = \"Yes\"" . ($allowTradesStatus == "Yes" ? " SELECTED" : "") . ">Yes</option>
+                <option value = \"No\"" . ($allowTradesStatus == "No" ? " SELECTED" : "") . ">No</option>
+            </select>
+            <INPUT type='submit' name='query' value='Set Allow Trades Status'>";
         break;
     case 'Playoffs':
         echo "<A HREF=\"updateAllTheThings.php\">Update All The Things</A><p>

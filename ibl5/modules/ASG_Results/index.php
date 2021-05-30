7283052d8d42cd1cca015c4487cf4c01c12d599f
A-Jay Nicolas <ajaynicolas@gmail.com>
2021-05-30 10:51:35 -0700

    refactor

--- ibl5/modules/ASG_Results/index.php
+++ ibl5/modules/ASG_Results/index.php
@@ -22,100 +22,48 @@ get_lang($module_name);
 $userpage = 1;
 include("header.php");
 
-$query2="select count(name) as votes,name from (select East_F1 as name from IBL_ASG_Votes union all select East_F2 from IBL_ASG_Votes union all select East_C from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
-$result2=mysql_query($query2);
-$num2=mysql_num_rows($result2);
+$queryEasternFrontcourt = "select count(name) as votes,name from (select East_F1 as name from IBL_ASG_Votes union all select East_F2 from IBL_ASG_Votes union all select East_C from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
+$queryEasternBackcourt = "select count(name) as votes,name from (select East_G1 as name from IBL_ASG_Votes union all select East_G2 from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
+$queryWesternFrontcourt = "select count(name) as votes,name from (select West_F1 as name from IBL_ASG_Votes union all select West_F2 from IBL_ASG_Votes union all select West_C from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
+$queryWesternBackcourt = "select count(name) as votes,name from (select West_G1 as name from IBL_ASG_Votes union all select West_G2 from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
 
-$query3="select count(name) as votes,name from (select East_G1 as name from IBL_ASG_Votes union all select East_G2 from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
-$result3=mysql_query($query3);
-$num3=mysql_num_rows($result3);
+function displayVotingResultsTable($query)
+{
+	$i = 0;
+	$result = mysql_query($query);
+	$num_rows = mysql_num_rows($result);
 
-$query5="select count(name) as votes,name from (select West_F1 as name from IBL_ASG_Votes union all select West_F2 from IBL_ASG_Votes union all select West_C from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
-$result5=mysql_query($query5);
-$num5=mysql_num_rows($result5);
+	while ($i < $num_rows) {
+		$player[$i] = mysql_result($result, $i, "name");
+		$votes[$i] = mysql_result($result, $i);
 
-$query6="select count(name) as votes,name from (select West_G1 as name from IBL_ASG_Votes union all select West_G2 from IBL_ASG_Votes) as tbl group by name having count(name) > 0 order by 1 desc;";
-$result6=mysql_query($query6);
-$num6=mysql_num_rows($result6);
+		$row .= "<tr><td>" . $player[$i] . "</td><td>" . $votes[$i] . "</td></tr>";
 
+		$i++;
+	}
 
-
-
-
-
-
+	echo "
+	<table class=\"sortable\" border=1>
+		<tr>
+			<th>Player</th>
+			<th>Votes</th>
+		</tr>
+		$row
+	</table>
+	<br><br>";
+}
 
 OpenTable();
-$h=0;
-$i=0;
-$n=0;
-$o=0;
 
-while ($h < $num2)
+echo "<h2>Eastern Conference Frontcourt</h2>";
+displayVotingResultsTable($queryEasternFrontcourt);
+echo "<h2>Eastern Conference Backcourt</h2>";
+displayVotingResultsTable($queryEasternBackcourt);
+echo "<h2>Western Conference Frontcourt</h2>";
+displayVotingResultsTable($queryWesternFrontcourt);
+echo "<h2>Western Conference Backcourt</h2>";
+displayVotingResultsTable($queryWesternBackcourt);
 
-{
-
-	$player[$h]=mysql_result($result2,$h, "name");
-	$votes[$h]=mysql_result($result2,$h);
-
-	$table_echo1=$table_echo1."<tr><td>".$player[$h]."</td><td>".$votes[$h]."</td></tr>";
-
-	$h++;
-}
-$text1=$text1."<table class=\"sortable\" border=1>
-		  <tr><th>Player</th><th> Votes</th></tr>$table_echo1</table><br><br>";
-
-while ($i < $num3)
-
-{
-
-	$player[$i]=mysql_result($result3,$i, "name");
-	$votes[$i]=mysql_result($result3,$i);
-
-	$table_echo2=$table_echo2."<tr><td>".$player[$i]."</td><td>".$votes[$i]."</td></tr>";
-
-	$i++;
-}
-$text2=$text2."<table class=\"sortable\" border=1>
-		  <tr><th>Player</th><th> Votes</th></tr>$table_echo2</table><br><br>";
-
-while ($n < $num5)
-
-{
-
-	$player[$n]=mysql_result($result5,$n, "name");
-	$votes[$n]=mysql_result($result5,$n);
-
-	$table_echo4=$table_echo4."<tr><td>".$player[$n]."</td><td>".$votes[$n]."</td></tr>";
-
-	$n++;
-}
-$text4=$text4."<table class=\"sortable\" border=1>
-		  <tr><th>Player</th><th> Votes</th></tr>$table_echo4</table><br><br>";
-
-while ($o < $num6)
-
-{
-
-	$player[$o]=mysql_result($result6,$o, "name");
-	$votes[$o]=mysql_result($result6,$o);
-
-	$table_echo5=$table_echo5."<tr><td>".$player[$o]."</td><td>".$votes[$o]."</td></tr>";
-
-	$o++;
-}
-$text5=$text5."<table class=\"sortable\" border=1>
-		  <tr><th>Player</th><th> Votes</th></tr>$table_echo5</table><br><br>";
-
-
-echo "<b>Eastern Conference Frontcourt</b>";
-echo $text1;
-echo "<b>Eastern Conference Backcourt</b>";
-echo $text2;
-echo "<b>Western Conference Frontcourt</b>";
-echo $text4;
-echo "<b>Western Conference Backcourt</b>";
-echo $text5;
 CloseTable();
 
 include("footer.php");

ed4d38b9c005808078fc53fb29c8f1fea79d66eb
A-Jay Nicolas <ajaynicolas@gmail.com>
2021-12-27 19:35:27 -0800

    PSR-12 spacing

--- ibl5/db/mysql.php
+++ ibl5/db/mysql.php
@@ -19,318 +19,253 @@
  *
  ***************************************************************************/
 
-if(!defined("SQL_LAYER"))
-{
+if (!defined("SQL_LAYER")) {
+	define("SQL_LAYER","mysql");
 
-define("SQL_LAYER","mysql");
-
-class sql_db
-{
-
-	var $db_connect_id;
-	var $query_result;
-	var $row = array();
-	var $rowset = array();
-	var $num_queries = 0;
-
-	//
-	// Constructor
-	//
-	function sql_db($sqlserver, $sqluser, $sqlpassword, $database, $persistency = true)
+	class sql_db
 	{
+		var $db_connect_id;
+		var $query_result;
+		var $row = array();
+		var $rowset = array();
+		var $num_queries = 0;
 
-		$this->persistency = $persistency;
-		$this->user = $sqluser;
-		$this->password = $sqlpassword;
-		$this->server = $sqlserver;
-		$this->dbname = $database;
+		//
+		// Constructor
+		//
+		function sql_db($sqlserver, $sqluser, $sqlpassword, $database, $persistency = true)
+		{
+			$this->persistency = $persistency;
+			$this->user = $sqluser;
+			$this->password = $sqlpassword;
+			$this->server = $sqlserver;
+			$this->dbname = $database;
 
-		if($this->persistency)
-		{
-			$this->db_connect_id = @mysqli_pconnect($this->server, $this->user, $this->password);
-		}
-		else
-		{
-			$this->db_connect_id = @mysqli_connect($this->server, $this->user, $this->password);
-		}
-		if($this->db_connect_id)
-		{
-			if($database != "")
-			{
-				$this->dbname = $database;
-				$dbselect = @mysqli_select_db($this->db_connect_id, $this->dbname);
-				if(!$dbselect)
-				{
-					@mysqli_close($this->db_connect_id);
-					$this->db_connect_id = $dbselect;
-				}
+			if ($this->persistency) {
+				$this->db_connect_id = @mysqli_pconnect($this->server, $this->user, $this->password);
+			} else {
+				$this->db_connect_id = @mysqli_connect($this->server, $this->user, $this->password);
 			}
-			return $this->db_connect_id;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	//
-	// Other base methods
-	//
-	function sql_close()
-	{
-		if($this->db_connect_id)
-		{
-			if($this->query_result)
-			{
-				@mysqli_free_result($this->query_result);
-			}
-			$result = @mysqli_close($this->db_connect_id);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	//
-	// Base query method
-	//
-	function sql_query($query = "", $transaction = FALSE)
-	{
-		// Remove any pre-existing queries
-		unset($this->query_result);
-		if($query != "")
-                {
-
-			$this->query_result = @mysqli_query($this->db_connect_id, $query);
-
-		}
-		if($this->query_result)
-		{
-			unset($this->row[$this->query_result]);
-				error_log($query);
-			unset($this->rowset[$this->query_result]);
-			return $this->query_result;
-		}
-		else
-		{
-			return ( $transaction == END_TRANSACTION ) ? true : false;
-		}
-	}
-
-	//
-	// Other query methods
-	//
-	function sql_numrows($query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$result = @mysqli_num_rows($query_id);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_affectedrows()
-	{
-		if($this->db_connect_id)
-		{
-			$result = @mysqli_affected_rows($this->db_connect_id);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_numfields($query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$result = @mysqli_num_fields($query_id);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_fieldname($offset, $query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$result = @mysqli_fetch_field_direct($query_id, $offset);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_fieldtype($offset, $query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$result = @mysqli_fetch_field_direct($query_id, $offset);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_fetchrow($query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$this->row[$query_id] = @mysqli_fetch_array($query_id);
-			return $this->row[$query_id];
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_fetchrowset($query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			unset($this->rowset[$query_id]);
-			unset($this->row[$query_id]);
-			while($this->rowset[$query_id] = @mysqli_fetch_array($query_id))
-			{
-				$result[] = $this->rowset[$query_id];
-			}
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_fetchfield($field, $rownum = -1, $query_id = 0)
-	{
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			if($rownum > -1)
-			{
-				$result = @mysqli_result($query_id, $rownum, $field);
-			}
-			else
-			{
-				if(empty($this->row[$query_id]) && empty($this->rowset[$query_id]))
-				{
-					if($this->sql_fetchrow())
-					{
-						$result = $this->row[$query_id][$field];
+			if ($this->db_connect_id) {
+				if ($database != "") {
+					$this->dbname = $database;
+					$dbselect = @mysqli_select_db($this->db_connect_id, $this->dbname);
+					if (!$dbselect) {
+						@mysqli_close($this->db_connect_id);
+						$this->db_connect_id = $dbselect;
 					}
 				}
-				else
-				{
-					if($this->rowset[$query_id])
-					{
-						$result = $this->rowset[$query_id][$field];
-					}
-					else if($this->row[$query_id])
-					{
-						$result = $this->row[$query_id][$field];
+				return $this->db_connect_id;
+			} else {
+				return false;
+			}
+		}
+
+		//
+		// Other base methods
+		//
+		function sql_close()
+		{
+			if ($this->db_connect_id) {
+				if ($this->query_result) {
+					@mysqli_free_result($this->query_result);
+				}
+				$result = @mysqli_close($this->db_connect_id);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		//
+		// Base query method
+		//
+		function sql_query($query = "", $transaction = FALSE)
+		{
+			// Remove any pre-existing queries
+			unset($this->query_result);
+			if ($query != "") {
+				$this->query_result = @mysqli_query($this->db_connect_id, $query);
+			}
+			if ($this->query_result) {
+				unset($this->row[$this->query_result]);
+					error_log($query);
+				unset($this->rowset[$this->query_result]);
+				return $this->query_result;
+			} else {
+				return ( $transaction == END_TRANSACTION ) ? true : false;
+			}
+		}
+
+		//
+		// Other query methods
+		//
+		function sql_numrows($query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$result = @mysqli_num_rows($query_id);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_affectedrows()
+		{
+			if ($this->db_connect_id) {
+				$result = @mysqli_affected_rows($this->db_connect_id);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_numfields($query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$result = @mysqli_num_fields($query_id);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_fieldname($offset, $query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$result = @mysqli_fetch_field_direct($query_id, $offset);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_fieldtype($offset, $query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$result = @mysqli_fetch_field_direct($query_id, $offset);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_fetchrow($query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$this->row[$query_id] = @mysqli_fetch_array($query_id);
+				return $this->row[$query_id];
+			} else {
+				return false;
+			}
+		}
+
+		function sql_fetchrowset($query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				unset($this->rowset[$query_id]);
+				unset($this->row[$query_id]);
+				while($this->rowset[$query_id] = @mysqli_fetch_array($query_id)) {
+					$result[] = $this->rowset[$query_id];
+				}
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_fetchfield($field, $rownum = -1, $query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				if ($rownum > -1) {
+					$result = @mysqli_result($query_id, $rownum, $field);
+				} else {
+					if (empty($this->row[$query_id]) && empty($this->rowset[$query_id])) {
+						if ($this->sql_fetchrow()) {
+							$result = $this->row[$query_id][$field];
+						}
+					} else {
+						if ($this->rowset[$query_id]) {
+							$result = $this->rowset[$query_id][$field];
+						} elseif ($this->row[$query_id]) {
+							$result = $this->row[$query_id][$field];
+						}
 					}
 				}
+				return $result;
+			} else {
+				return false;
 			}
+		}
+
+		function sql_rowseek($rownum, $query_id = 0){
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				$result = @mysqli_data_seek($query_id, $rownum);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_nextid()
+		{
+			if ($this->db_connect_id) {
+				$result = @mysqli_insert_id($this->db_connect_id);
+				return $result;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_freeresult($query_id = 0)
+		{
+			if (!$query_id) {
+				$query_id = $this->query_result;
+			}
+			if ($query_id) {
+				unset($this->row[$query_id]);
+				unset($this->rowset[$query_id]);
+
+				@mysqli_free_result($query_id);
+
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		function sql_error($query_id = 0)
+		{
+			$result["message"] = @mysqli_error($this->db_connect_id);
+			$result["code"] = @mysqli_errno($this->db_connect_id);
+
 			return $result;
 		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_rowseek($rownum, $query_id = 0){
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-		if($query_id)
-		{
-			$result = @mysqli_data_seek($query_id, $rownum);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_nextid(){
-		if($this->db_connect_id)
-		{
-			$result = @mysqli_insert_id($this->db_connect_id);
-			return $result;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_freeresult($query_id = 0){
-		if(!$query_id)
-		{
-			$query_id = $this->query_result;
-		}
-
-		if ( $query_id )
-		{
-			unset($this->row[$query_id]);
-			unset($this->rowset[$query_id]);
-
-			@mysqli_free_result($query_id);
-
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	function sql_error($query_id = 0)
-	{
-		$result["message"] = @mysqli_error($this->db_connect_id);
-		$result["code"] = @mysqli_errno($this->db_connect_id);
-
-		return $result;
-	}
-
-} // class sql_db
-
+	} // class sql_db
 } // if ... define
 
 ?>

f7344fadca0562352cc6bf6f02d1a6636f64affb
A-Jay Nicolas <ajaynicolas@gmail.com>
2021-12-30 11:28:04 -0800

    move autoloader code earlier to autoload Db classes

--- ibl5/mainfile.php
+++ ibl5/mainfile.php
@@ -197,6 +197,40 @@ if(!$dbname) {
     die("<br><br><center><img src=images/logo.gif><br><br><b>There seems that PHP-Nuke isn't installed yet.<br>(The values in config.php file are the default ones)<br><br>You can proceed with the <a href='./install/index.php'>web installation</a> now.</center></b>");
 }
 
+function mlaphp_autoloader($class)
+{
+    // strip off any leading namespace separator from PHP 5.3
+    $class = ltrim($class, '\\');
+
+    // the eventual file path
+    $subpath = '';
+
+    // is there a PHP 5.3 namespace separator?
+    $pos = strrpos($class, '\\');
+    if ($pos !== false) {
+        // convert namespace separators to directory separators
+        $ns = substr($class, 0, $pos);
+        $subpath = str_replace('\\', DIRECTORY_SEPARATOR, $ns)
+                 . DIRECTORY_SEPARATOR;
+        // remove the namespace portion from the final class name portion
+        $class = substr($class, $pos + 1);
+    }
+
+    // convert underscores in the class name to directory separators
+    $subpath .= str_replace('_', DIRECTORY_SEPARATOR, $class);
+
+    // the path to our central class directory location
+    $dir = 'classes';
+
+    // prefix with the central directory location and suffix with .php,
+    // then require it.
+    $file = $dir . DIRECTORY_SEPARATOR . $subpath . '.php';
+    require $file;
+}
+
+// register it with SPL
+spl_autoload_register('mlaphp_autoloader');
+
 @require_once(INCLUDE_PATH."db/db.php");
 
 /* FOLLOWING TWO LINES ARE DEPRECATED BUT ARE HERE FOR OLD MODULES COMPATIBILITY */
@@ -300,40 +334,6 @@ if (!defined('FORUM_ADMIN')) {
 		$currentlang = $language;
 	}
 }
-
-function mlaphp_autoloader($class)
-{
-    // strip off any leading namespace separator from PHP 5.3
-    $class = ltrim($class, '\\');
-
-    // the eventual file path
-    $subpath = '';
-
-    // is there a PHP 5.3 namespace separator?
-    $pos = strrpos($class, '\\');
-    if ($pos !== false) {
-        // convert namespace separators to directory separators
-        $ns = substr($class, 0, $pos);
-        $subpath = str_replace('\\', DIRECTORY_SEPARATOR, $ns)
-                 . DIRECTORY_SEPARATOR;
-        // remove the namespace portion from the final class name portion
-        $class = substr($class, $pos + 1);
-    }
-
-    // convert underscores in the class name to directory separators
-    $subpath .= str_replace('_', DIRECTORY_SEPARATOR, $class);
-
-    // the path to our central class directory location
-    $dir = 'classes';
-
-    // prefix with the central directory location and suffix with .php,
-    // then require it.
-    $file = $dir . DIRECTORY_SEPARATOR . $subpath . '.php';
-    require $file;
-}
-
-// register it with SPL
-spl_autoload_register('mlaphp_autoloader');
 
 function makePass() {
 	$cons = "bcdfghjklmnpqrstvwxyz";

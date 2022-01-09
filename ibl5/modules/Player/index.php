9e712a51c0249e9c0e1357ac325dc08389966a3b
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-01-09 13:03:26 -0800

    formatting: awards()

--- ibl5/modules/Player/index.php
+++ ibl5/modules/Player/index.php
@@ -831,13 +831,18 @@ function awards()
     echo "Partial matches on a name or award are okay and are <b>not</b> case sensitive (e.g., entering \"Dard\" will match with \"Darden\" and \"Bedard\").<p>
 
         <form name=\"Search\" method=\"post\" action=\"modules.php?name=Player&pa=awards\">
-        <table border=1><tr><td>
-        <td>NAME: <input type=\"text\" name=\"aw_name\" size=\"32\" value=\"$as_name\"></td>
-        <td>AWARD: <input type=\"text\" name=\"aw_Award\" size=\"32\" value=\"$as_Award\"></td>
-        <td>Year: <input type=\"text\" name=\"aw_year\" size=\"4\" value=\"$as_year\"></td></tr>
-        <tr><td colspan=3>SORT BY:";
+        <table border=1>
+            <tr>
+                <td>NAME: <input type=\"text\" name=\"aw_name\" size=\"32\" value=\"$as_name\"></td>
+                <td>AWARD: <input type=\"text\" name=\"aw_Award\" size=\"32\" value=\"$as_Award\"></td>
+                <td>Year: <input type=\"text\" name=\"aw_year\" size=\"4\" value=\"$as_year\"></td>
+            </tr>
+            <tr>
+                <td colspan=3>SORT BY:";
 
-    if ($as_sortby == NULL) $sortby=3;
+    if ($as_sortby == NULL) {
+        $sortby = 3;
+    }
 
     if ($as_sortby == 1) {
         echo "<input type=\"radio\" name=\"aw_sortby\" value=\"1\" checked> Name |";
@@ -862,67 +867,83 @@ function awards()
 
     // ========= SET QUERY BASED ON SEARCH PARAMETERS
 
-    $continuequery=0;
-    $query="SELECT * FROM nuke_ibl_awards";
+    $continuequery = 0;
+    $query = "SELECT * FROM nuke_ibl_awards";
 
     if ($as_year != NULL) {
-        $query=$query." WHERE year = '$as_year'";
-        $continuequery=1;
+        $query .= " WHERE year = '$as_year'";
+        $continuequery = 1;
     }
 
     if ($continuequery == 0) {
         if ($as_Award != NULL) {
-            $query=$query." WHERE Award LIKE '%$as_Award%'";
-            $continuequery=1;
+            $query .= " WHERE Award LIKE '%$as_Award%'";
+            $continuequery = 1;
         }
     } else {
         if ($as_Award != NULL) {
-            $query=$query." AND Award LIKE '%$as_Award%'";
+            $query .= " AND Award LIKE '%$as_Award%'";
         }
     }
 
     if ($continuequery == 0) {
         if ($as_name != NULL) {
-            $query=$query." WHERE name LIKE '%$as_name%'";
-            $continuequery=1;
+            $query .= " WHERE name LIKE '%$as_name%'";
+            $continuequery = 1;
         }
     } else {
         if ($as_name != NULL) {
-            $query=$query." AND name LIKE '%$as_name%'";
+            $query .= " AND name LIKE '%$as_name%'";
         }
     }
 
-    $orderby='Year';
+    $orderby = 'Year';
 
-    if ($as_sortby == 1) $orderby='name';
-    if ($as_sortby == 2) $orderby='Award';
-    if ($as_sortby == 3) $orderby='year';
+    if ($as_sortby == 1) {
+        $orderby = 'name';
+    }
+    if ($as_sortby == 2) {
+        $orderby = 'Award';
+    }
+    if ($as_sortby == 3) {
+        $orderby = 'year';
+    }
 
-    $query=$query." ORDER BY $orderby ASC";
+    $query .= " ORDER BY $orderby ASC";
 
     // =============== EXECUTE QUERY
 
-    $result=mysql_query($query);
-    @$num=mysql_numrows($result);
+    $result = mysql_query($query);
+    @$num = mysql_numrows($result);
 
     echo "<table border=1 cellpadding=0 cellspacing=0>
-        <tr><td colspan=3><center><i>Search Results</i></center></td></tr>
-        <tr><th>Year</th><th>Player</th><th>Award</th></tr>";
+        <tr>
+            <td colspan=3><center><i>Search Results</i></center></td>
+        </tr>
+        <tr>
+            <th>Year</th>
+            <th>Player</th>
+            <th>Award</th>
+        </tr>";
 
     // ========== FILL RESULTS
 
-    $i=0;
+    $i = 0;
 
     while ($i < $num) {
-        $a_name=mysql_result($result,$i,"name");
-        $a_Award=mysql_result($result,$i,"Award");
-        $a_year=mysql_result($result,$i,"year");
+        $a_name = mysql_result($result, $i, "name");
+        $a_Award = mysql_result($result, $i, "Award");
+        $a_year = mysql_result($result, $i, "year");
         if ($i % 2) {
             echo "<tr bgcolor=#ffffff>";
         } else {
             echo "<tr bgcolor=#e6e7e2>";
         }
-        echo "<tr><td><center>$a_year</center></td><td><center>$a_name</a></center></td><td><center>$a_Award</center></td></tr>";
+        echo "<tr>
+            <td><center>$a_year</center></td>
+            <td><center>$a_name</a></center></td>
+            <td><center>$a_Award</center></td>
+        </tr>";
 
         $i++;
     }

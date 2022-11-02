48ce24145feb01f66890882ca8a16fd7cbcd3ac1
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-11-02 11:08:15 -0700

    fix Player page links

--- ibl5/draft.php
+++ ibl5/draft.php
@@ -57,7 +57,7 @@ if ($num == 0) {
 
         echo "<td>$round</td>
             <td>$draftpickno</td>
-            <td><a href=\"../modules.php?name=Player&pa=showpage&pid=$pid\">$name</a></td>
+            <td><a href=\"../ibl5/modules.php?name=Player&pa=showpage&pid=$pid\">$name</a></td>
             <td>$draftedby</td>
             <td><img height=50 src=\"../ibl5/images/player/$pid.jpg\"></td>
             <td>$college</td>

1ed047415241e0636f3c5cd73ee1f629a8dbe0bd
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-11-25 15:06:01 -0800

    get the old draft page working again

--- ibl5/draft_selection.php
+++ ibl5/draft_selection.php
@@ -6,14 +6,25 @@ $teamname = $_POST['teamname'];
 $player = $_POST['player'];
 $draft_round = $_POST['draft_round'];
 $draft_pick = $_POST['draft_pick'];
+$date = date('Y-m-d h:m:s');
 
-$query = "UPDATE ibl_draft SET `player` = '$player' WHERE `round` = '$draft_round' AND `pick` = '$draft_pick'";
-$result = $db->sql_query($query);
-
-$query2 = "UPDATE `ibl_scout_rookieratings` SET `drafted` = '1' WHERE `name` = '$player'";
-$result2 = $db->sql_query($query2);
-//$system_date = time() + (7 * 24 * 60 * 60);
-//echo 'Now:       '. date('Y-m-d h:m:s') ."<br>";
-
-echo "With pick number $draft_pick in round $draft_round $teamname select $player!<br>
-Go back to <a href='../'>home page</a>";
+if ($player != NULL) {
+    $query = "UPDATE ibl_draft 
+        SET `player` = '$player', 
+            `date` = '$date' 
+        WHERE `round` = '$draft_round' 
+           AND `pick` = '$draft_pick'";
+    $result = $db->sql_query($query);
+    
+    $query2 = "UPDATE `ibl_scout_rookieratings`
+        SET `team` = '$teamname', 
+            `drafted` = '1'
+        WHERE `name` = '$player'";
+    $result2 = $db->sql_query($query2);
+    
+    echo "With pick number $draft_pick in round $draft_round $teamname select $player!<br>
+    <a href=\"modules.php?name=College_Scouting\">Go back to the Draft module</a>";
+} else {
+    echo "Oops, you didn't select a player.<p>
+        Please <a href=\"modules.php?name=College_Scouting\">go back to the Draft module</a> and select a player before hitting the Draft button.";
+}

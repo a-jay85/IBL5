2ee6f9b849de28baf9f9d9fbde016dcf1f900c08
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-12-11 21:53:15 -0800

    initial commit of IBLbot

---
+++ ibl5/IBLbot/index.js
@@ -0,0 +1,29 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
+app.listen (port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// client.users.send('283183467804491776', 'test');

dbd0aa6482a460358662895331bb27f50d348860
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-12-19 21:20:29 -0800

    send Discord DM to opposite team when trade is declined

--- ibl5/rejecttradeoffer.php
+++ ibl5/rejecttradeoffer.php
@@ -1,11 +1,26 @@
 <?php
 
 require 'mainfile.php';
+$sharedFunctions = new Shared($db);
 
 $offer_id = $_POST['offer'];
+$teamRejecting = $_POST['teamRejecting'];
+$teamReceiving = $_POST['teamReceiving'];
 
 $queryclear = "DELETE FROM ibl_trade_info WHERE `tradeofferid` = '$offer_id'";
 $resultclear = $db->sql_query($queryclear);
+
+$rejectingUserDiscordID = $sharedFunctions->getDiscordIDFromTeamname($teamRejecting);
+$receivingUserDiscordID = $sharedFunctions->getDiscordIDFromTeamname($teamReceiving);
+$discordDMmessage = 'Sorry, trade proposal declined by <@!' . $rejectingUserDiscordID . '>!
+
+Go here to make another offer: http://www.iblhoops.net/ibl5/modules.php?name=Trading&op=reviewtrade';
+$arrayContent = array(
+        'message' => $discordDMmessage,
+        'receivingUserDiscordID' => $receivingUserDiscordID,);
+
+echo "<p>";
+$response = Discord::sendCurlPOST('http://localhost:50000/discordDM', $arrayContent);
 
 ?>
 

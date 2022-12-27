fbf099ebe36a61385d42b97f23835ea5694231ba
A-Jay Nicolas <ajaynicolas@gmail.com>
2022-12-27 12:54:07 -0800

    update ASG voting table and column names

--- ibl5/asg_vote_reset.php
+++ ibl5/asg_vote_reset.php
@@ -2,7 +2,11 @@
 
 require 'mainfile.php';
 
-$query1 = "UPDATE ibl_ASG_Votes SET East_C = NULL, East_F1 = NULL, East_F2 = NULL, East_G1 = NULL, East_G2 = NULL, West_C = NULL, West_F1 = NULL, West_F2 = NULL, West_G1 = NULL, West_G2 = NULL";
+$query1 = "UPDATE ibl_votes_ASG 
+    SET East_F1 = NULL, East_F2 = NULL, East_F3 = NULL, East_F4 = NULL,
+        West_F1 = NULL, West_F2 = NULL, West_F3 = NULL, West_F4 = NULL,
+        East_B1 = NULL, East_B2 = NULL, East_B3 = NULL, East_B4 = NULL,
+        West_B1 = NULL, West_B2 = NULL, West_B3 = NULL, West_B4 = NULL;";
 $result1 = $db->sql_query($query1);
 
 $query2 = "UPDATE ibl_settings SET value = 'Yes' where name = 'ASG Voting'";

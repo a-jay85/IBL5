8eb887ab57f80c4158fa27f64d3c7533d2a3e7cb
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-07-13 11:37:46 -0700

    remove unused stats

--- ibl5/blocks/block-Leaders.php
+++ ibl5/blocks/block-Leaders.php
@@ -21,17 +21,16 @@ global $db;
 $query = "SELECT * FROM ibl_plr WHERE retired = 0 AND name NOT LIKE \"%Buyouts%\" ORDER BY ordinal ASC";
 $result = $db->sql_query($query);
 $num = $db->sql_numrows($result);
-$i = 0;
-$name1 = $name2 = $name3 = $name4 = $name5 = "";
 
+$name1 = $name2 = $name3 = $name4 = $name5 = "";
 $ppg1 = $ppg2 = $ppg3 = $ppg4 = $ppg5 = 0;
 $reb1 = $reb2 = $reb3 = $reb4 = $reb5 = 0;
 $ast1 = $ast2 = $ast3 = $ast4 = $ast5 = 0;
 $stl1 = $stl2 = $stl3 = $stl4 = $stl5 = 0;
 $blk1 = $blk2 = $blk3 = $blk4 = $blk5 = 0;
 
+$i = 0;
 while ($i < $num) {
-
     $name = $db->sql_result($result, $i, "name");
     $p_ord = $db->sql_result($result, $i, "ordinal");
     $pid = $db->sql_result($result, $i, "pid");
@@ -39,25 +38,16 @@ while ($i < $num) {
     $teamname = $db->sql_result($result, $i, "teamname");
 
     $stats_gm = $db->sql_result($result, $i, "stats_gm");
+
     $stats_fgm = $db->sql_result($result, $i, "stats_fgm");
-    $stats_fga = $db->sql_result($result, $i, "stats_fga");
     $stats_ftm = $db->sql_result($result, $i, "stats_ftm");
-    $stats_fta = $db->sql_result($result, $i, "stats_fta");
     $stats_tgm = $db->sql_result($result, $i, "stats_3gm");
-    $stats_tga = $db->sql_result($result, $i, "stats_3ga");
 
     $stats_orb = $db->sql_result($result, $i, "stats_orb");
     $stats_drb = $db->sql_result($result, $i, "stats_drb");
-
     $stats_ast = $db->sql_result($result, $i, "stats_ast");
-
     $stats_stl = $db->sql_result($result, $i, "stats_stl");
-
     $stats_blk = $db->sql_result($result, $i, "stats_blk");
-
-    $stats_to = $db->sql_result($result, $i, "stats_to");
-
-    $stats_pf = $db->sql_result($result, $i, "stats_pf");
 
     $stats_reb = $stats_orb + $stats_drb;
     $stats_pts = 2 * $stats_fgm + $stats_ftm + $stats_tgm;
@@ -67,24 +57,16 @@ while ($i < $num) {
     @$stats_ast = ($stats_ast / $stats_gm);
     @$stats_stl = ($stats_stl / $stats_gm);
     @$stats_blk = ($stats_blk / $stats_gm);
-    @$stats_fgm = ($stats_fgm / $stats_gm);
-    @$stats_ftm = ($stats_ftm / $stats_gm);
-    @$stats_tgm = ($stats_tgm / $stats_gm);
-    @$stats_to = ($stats_to / $stats_gm);
-    @$stats_pf = ($stats_pf / $stats_gm);
 
     $stats_ppg = round($stats_ppg, 2);
     $stats_reb = round($stats_reb, 2);
     $stats_ast = round($stats_ast, 2);
     $stats_stl = round($stats_stl, 2);
     $stats_blk = round($stats_blk, 2);
-    $stats_fgm = round($stats_fgm, 2);
-    $stats_ftm = round($stats_ftm, 2);
-    $stats_3gm = round($stats_3gm, 2);
-    $stats_to = round($stats_to, 2);
-    $stats_pf = round($stats_pf, 2);
 
     if ($stats_ppg > $ppg1) {
+//----PPG---
+
         $ppg5 = $ppg4;
         $ppg4 = $ppg3;
         $ppg3 = $ppg2;
@@ -502,426 +484,6 @@ while ($i < $num) {
         $tidblk5 = $tid;
     }
 
-    //----FGM---
-
-    if ($stats_fgm > $fgm1) {
-        $fgm5 = $fgm4;
-        $fgm4 = $fgm3;
-        $fgm3 = $fgm2;
-        $fgm2 = $fgm1;
-        $fgm1 = $stats_fgm;
-        $name_fgm5 = $name_fgm4;
-        $name_fgm4 = $name_fgm3;
-        $name_fgm3 = $name_fgm2;
-        $name_fgm2 = $name_fgm1;
-        $name_fgm1 = $name;
-        $teamname_fgm5 = $teamname_fgm4;
-        $teamname_fgm4 = $teamname_fgm3;
-        $teamname_fgm3 = $teamname_fgm2;
-        $teamname_fgm2 = $teamname_fgm1;
-        $teamname_fgm1 = $teamname;
-        $pidfgm5 = $pidfgm4;
-        $pidfgm4 = $pidfgm3;
-        $pidfgm3 = $pidfgm2;
-        $pidfgm2 = $pidfgm1;
-        $pidfgm1 = $pid;
-        $tidfgm5 = $tidfgm4;
-        $tidfgm4 = $tidfgm3;
-        $tidfgm3 = $tidfgm2;
-        $tidfgm2 = $tidfgm1;
-        $tidfgm1 = $tid;
-    } elseif ($stats_fgm > $fgm2) {
-        $fgm5 = $fgm4;
-        $fgm4 = $fgm3;
-        $fgm3 = $fgm2;
-        $fgm2 = $stats_fgm;
-        $name_fgm5 = $name_fgm4;
-        $name_fgm4 = $name_fgm3;
-        $name_fgm3 = $name_fgm2;
-        $name_fgm2 = $name;
-        $teamname_fgm5 = $teamname_fgm4;
-        $teamname_fgm4 = $teamname_fgm3;
-        $teamname_fgm3 = $teamname_fgm2;
-        $teamname_fgm2 = $teamname;
-        $pidfgm5 = $pidfgm4;
-        $pidfgm4 = $pidfgm3;
-        $pidfgm3 = $pidfgm2;
-        $pidfgm2 = $pid;
-        $tidfgm5 = $tidfgm4;
-        $tidfgm4 = $tidfgm3;
-        $tidfgm3 = $tidfgm2;
-        $tidfgm2 = $tid;
-    } elseif ($stats_fgm > $fgm3) {
-        $fgm5 = $fgm4;
-        $fgm4 = $fgm3;
-        $fgm3 = $stats_fgm;
-        $name_fgm5 = $name_fgm4;
-        $name_fgm4 = $name_fgm3;
-        $name_fgm3 = $name;
-        $teamname_fgm5 = $teamname_fgm4;
-        $teamname_fgm4 = $teamname_fgm3;
-        $teamname_fgm3 = $teamname;
-        $pidfgm5 = $pidfgm4;
-        $pidfgm4 = $pidfgm3;
-        $pidfgm3 = $pid;
-        $tidfgm5 = $tidfgm4;
-        $tidfgm4 = $tidfgm3;
-        $tidfgm3 = $tid;
-    } elseif ($stats_fgm > $fgm4) {
-        $fgm5 = $fgm4;
-        $fgm4 = $stats_fgm;
-        $name_fgm5 = $name_fgm4;
-        $name_fgm4 = $name;
-        $teamname_fgm5 = $teamname_fgm4;
-        $teamname_fgm4 = $teamname;
-        $pidfgm5 = $pidfgm4;
-        $pidfgm4 = $pid;
-        $tidfgm5 = $tidfgm4;
-        $tidfgm4 = $tid;
-    } elseif ($stats_fgm > $fgm5) {
-        $fgm5 = $stats_fgm;
-        $name_fgm5 = $name;
-        $teamname_fgm5 = $teamname;
-        $pidfgm5 = $pid;
-        $tidfgm5 = $tid;
-    }
-
-    //----FTM---
-
-    if ($stats_ftm > $ftm1) {
-        $ftm5 = $ftm4;
-        $ftm4 = $ftm3;
-        $ftm3 = $ftm2;
-        $ftm2 = $ftm1;
-        $ftm1 = $stats_ftm;
-        $name_ftm5 = $name_ftm4;
-        $name_ftm4 = $name_ftm3;
-        $name_ftm3 = $name_ftm2;
-        $name_ftm2 = $name_ftm1;
-        $name_ftm1 = $name;
-        $teamname_ftm5 = $teamname_ftm4;
-        $teamname_ftm4 = $teamname_ftm3;
-        $teamname_ftm3 = $teamname_ftm2;
-        $teamname_ftm2 = $teamname_ftm1;
-        $teamname_ftm1 = $teamname;
-        $pidftm5 = $pidftm4;
-        $pidftm4 = $pidftm3;
-        $pidftm3 = $pidftm2;
-        $pidftm2 = $pidftm1;
-        $pidftm1 = $pid;
-        $tidftm5 = $tidftm4;
-        $tidftm4 = $tidftm3;
-        $tidftm3 = $tidftm2;
-        $tidftm2 = $tidftm1;
-        $tidftm1 = $tid;
-    } elseif ($stats_ftm > $ftm2) {
-        $ftm5 = $ftm4;
-        $ftm4 = $ftm3;
-        $ftm3 = $ftm2;
-        $ftm2 = $stats_ftm;
-        $name_ftm5 = $name_ftm4;
-        $name_ftm4 = $name_ftm3;
-        $name_ftm3 = $name_ftm2;
-        $name_ftm2 = $name;
-        $teamname_ftm5 = $teamname_ftm4;
-        $teamname_ftm4 = $teamname_ftm3;
-        $teamname_ftm3 = $teamname_ftm2;
-        $teamname_ftm2 = $teamname;
-        $pidftm5 = $pidftm4;
-        $pidftm4 = $pidftm3;
-        $pidftm3 = $pidftm2;
-        $pidftm2 = $pid;
-        $tidftm5 = $tidftm4;
-        $tidftm4 = $tidftm3;
-        $tidftm3 = $tidftm2;
-        $tidftm2 = $tid;
-    } elseif ($stats_ftm > $ftm3) {
-        $ftm5 = $ftm4;
-        $ftm4 = $ftm3;
-        $ftm3 = $stats_ftm;
-        $name_ftm5 = $name_ftm4;
-        $name_ftm4 = $name_ftm3;
-        $name_ftm3 = $name;
-        $teamname_ftm5 = $teamname_ftm4;
-        $teamname_ftm4 = $teamname_ftm3;
-        $teamname_ftm3 = $teamname;
-        $pidftm5 = $pidftm4;
-        $pidftm4 = $pidftm3;
-        $pidftm3 = $pid;
-        $tidftm5 = $tidftm4;
-        $tidftm4 = $tidftm3;
-        $tidftm3 = $tid;
-    } elseif ($stats_ftm > $ftm4) {
-        $ftm5 = $ftm4;
-        $ftm4 = $stats_ftm;
-        $name_ftm5 = $name_ftm4;
-        $name_ftm4 = $name;
-        $teamname_ftm5 = $teamname_ftm4;
-        $teamname_ftm4 = $teamname;
-        $pidftm5 = $pidftm4;
-        $pidftm4 = $pid;
-        $tidftm5 = $tidftm4;
-        $tidftm4 = $tid;
-    } elseif ($stats_ftm > $ftm5) {
-        $ftm5 = $stats_ftm;
-        $name_ftm5 = $name;
-        $teamname_ftm5 = $teamname;
-        $pidftm5 = $pid;
-        $tidftm5 = $tid;
-    }
-
-    //----3GM---
-
-    if ($stats_tgm > $tgm1) {
-        $tgm5 = $tgm4;
-        $tgm4 = $tgm3;
-        $tgm3 = $tgm2;
-        $tgm2 = $tgm1;
-        $tgm1 = $stats_tgm;
-        $name_tgm5 = $name_tgm4;
-        $name_tgm4 = $name_tgm3;
-        $name_tgm3 = $name_tgm2;
-        $name_tgm2 = $name_tgm1;
-        $name_tgm1 = $name;
-        $teamname_tgm5 = $teamname_tgm4;
-        $teamname_tgm4 = $teamname_tgm3;
-        $teamname_tgm3 = $teamname_tgm2;
-        $teamname_tgm2 = $teamname_tgm1;
-        $teamname_tgm1 = $teamname;
-        $pidtgm5 = $pidtgm4;
-        $pidtgm4 = $pidtgm3;
-        $pidtgm3 = $pidtgm2;
-        $pidtgm2 = $pidtgm1;
-        $pidtgm1 = $pid;
-        $tidtgm5 = $tidtgm4;
-        $tidtgm4 = $tidtgm3;
-        $tidtgm3 = $tidtgm2;
-        $tidtgm2 = $tidtgm1;
-        $tidtgm1 = $tid;
-    } elseif ($stats_tgm > $tgm2) {
-        $tgm5 = $tgm4;
-        $tgm4 = $tgm3;
-        $tgm3 = $tgm2;
-        $tgm2 = $stats_tgm;
-        $name_tgm5 = $name_tgm4;
-        $name_tgm4 = $name_tgm3;
-        $name_tgm3 = $name_tgm2;
-        $name_tgm2 = $name;
-        $teamname_tgm5 = $teamname_tgm4;
-        $teamname_tgm4 = $teamname_tgm3;
-        $teamname_tgm3 = $teamname_tgm2;
-        $teamname_tgm2 = $teamname;
-        $pidtgm5 = $pidtgm4;
-        $pidtgm4 = $pidtgm3;
-        $pidtgm3 = $pidtgm2;
-        $pidtgm2 = $pid;
-        $tidtgm5 = $tidtgm4;
-        $tidtgm4 = $tidtgm3;
-        $tidtgm3 = $tidtgm2;
-        $tidtgm2 = $tid;
-    } elseif ($stats_tgm > $tgm3) {
-        $tgm5 = $tgm4;
-        $tgm4 = $tgm3;
-        $tgm3 = $stats_tgm;
-        $name_tgm5 = $name_tgm4;
-        $name_tgm4 = $name_tgm3;
-        $name_tgm3 = $name;
-        $teamname_tgm5 = $teamname_tgm4;
-        $teamname_tgm4 = $teamname_tgm3;
-        $teamname_tgm3 = $teamname;
-        $pidtgm5 = $pidtgm4;
-        $pidtgm4 = $pidtgm3;
-        $pidtgm3 = $pid;
-        $tidtgm5 = $tidtgm4;
-        $tidtgm4 = $tidtgm3;
-        $tidtgm3 = $tid;
-    } elseif ($stats_tgm > $tgm4) {
-        $tgm5 = $tgm4;
-        $tgm4 = $stats_tgm;
-        $name_tgm5 = $name_tgm4;
-        $name_tgm4 = $name;
-        $teamname_tgm5 = $teamname_tgm4;
-        $teamname_tgm4 = $teamname;
-        $pidtgm5 = $pidtgm4;
-        $pidtgm4 = $pid;
-        $tidtgm5 = $tidtgm4;
-        $tidtgm4 = $tid;
-    } elseif ($stats_tgm > $tgm5) {
-        $tgm5 = $stats_tgm;
-        $name_tgm5 = $name;
-        $teamname_tgm5 = $teamname;
-        $pidtgm5 = $pid;
-        $tidtgm5 = $tid;
-    }
-
-    //----TVR---
-
-    if ($stats_to > $to1) {
-        $to5 = $to4;
-        $to4 = $to3;
-        $to3 = $to2;
-        $to2 = $to1;
-        $to1 = $stats_to;
-        $name_to5 = $name_to4;
-        $name_to4 = $name_to3;
-        $name_to3 = $name_to2;
-        $name_to2 = $name_to1;
-        $name_to1 = $name;
-        $teamname_to5 = $teamname_to4;
-        $teamname_to4 = $teamname_to3;
-        $teamname_to3 = $teamname_to2;
-        $teamname_to2 = $teamname_to1;
-        $teamname_to1 = $teamname;
-        $pidto5 = $pidto4;
-        $pidto4 = $pidto3;
-        $pidto3 = $pidto2;
-        $pidto2 = $pidto1;
-        $pidto1 = $pid;
-        $tidto5 = $tidto4;
-        $tidto4 = $tidto3;
-        $tidto3 = $tidto2;
-        $tidto2 = $tidto1;
-        $tidto1 = $tid;
-    } elseif ($stats_to > $to2) {
-        $to5 = $to4;
-        $to4 = $to3;
-        $to3 = $to2;
-        $to2 = $stats_to;
-        $name_to5 = $name_to4;
-        $name_to4 = $name_to3;
-        $name_to3 = $name_to2;
-        $name_to2 = $name;
-        $teamname_to5 = $teamname_to4;
-        $teamname_to4 = $teamname_to3;
-        $teamname_to3 = $teamname_to2;
-        $teamname_to2 = $teamname;
-        $pidto5 = $pidto4;
-        $pidto4 = $pidto3;
-        $pidto3 = $pidto2;
-        $pidto2 = $pid;
-        $tidto5 = $tidto4;
-        $tidto4 = $tidto3;
-        $tidto3 = $tidto2;
-        $tidto2 = $tid;
-    } elseif ($stats_to > $to3) {
-        $to5 = $to4;
-        $to4 = $to3;
-        $to3 = $stats_to;
-        $name_to5 = $name_to4;
-        $name_to4 = $name_to3;
-        $name_to3 = $name;
-        $teamname_to5 = $teamname_to4;
-        $teamname_to4 = $teamname_to3;
-        $teamname_to3 = $teamname;
-        $pidto5 = $pidto4;
-        $pidto4 = $pidto3;
-        $pidto3 = $pid;
-        $tidto5 = $tidto4;
-        $tidto4 = $tidto3;
-        $tidto3 = $tid;
-    } elseif ($stats_to > $to4) {
-        $to5 = $to4;
-        $to4 = $stats_to;
-        $name_to5 = $name_to4;
-        $name_to4 = $name;
-        $teamname_to5 = $teamname_to4;
-        $teamname_to4 = $teamname;
-        $pidto5 = $pidto4;
-        $pidto4 = $pid;
-        $tidto5 = $tidto4;
-        $tidto4 = $tid;
-    } elseif ($stats_to > $to5) {
-        $to5 = $stats_to;
-        $name_to5 = $name;
-        $teamname_to5 = $teamname;
-        $pidto5 = $pid;
-        $tidto5 = $tid;
-    }
-
-    //----FLS---
-
-    if ($stats_pf > $pf1) {
-        $pf5 = $pf4;
-        $pf4 = $pf3;
-        $pf3 = $pf2;
-        $pf2 = $pf1;
-        $pf1 = $stats_pf;
-        $name_pf5 = $name_pf4;
-        $name_pf4 = $name_pf3;
-        $name_pf3 = $name_pf2;
-        $name_pf2 = $name_pf1;
-        $name_pf1 = $name;
-        $teamname_pf5 = $teamname_pf4;
-        $teamname_pf4 = $teamname_pf3;
-        $teamname_pf3 = $teamname_pf2;
-        $teamname_pf2 = $teamname_pf1;
-        $teamname_pf1 = $teamname;
-        $pidpf5 = $pidpf4;
-        $pidpf4 = $pidpf3;
-        $pidpf3 = $pidpf2;
-        $pidpf2 = $pidpf1;
-        $pidpf1 = $pid;
-        $tidpf5 = $tidpf4;
-        $tidpf4 = $tidpf3;
-        $tidpf3 = $tidpf2;
-        $tidpf2 = $tidpf1;
-        $tidpf1 = $tid;
-    } elseif ($stats_pf > $pf2) {
-        $pf5 = $pf4;
-        $pf4 = $pf3;
-        $pf3 = $pf2;
-        $pf2 = $stats_pf;
-        $name_pf5 = $name_pf4;
-        $name_pf4 = $name_pf3;
-        $name_pf3 = $name_pf2;
-        $name_pf2 = $name;
-        $teamname_pf5 = $teamname_pf4;
-        $teamname_pf4 = $teamname_pf3;
-        $teamname_pf3 = $teamname_pf2;
-        $teamname_pf2 = $teamname;
-        $pidpf5 = $pidpf4;
-        $pidpf4 = $pidpf3;
-        $pidpf3 = $pidpf2;
-        $pidpf2 = $pid;
-        $tidpf5 = $tidpf4;
-        $tidpf4 = $tidpf3;
-        $tidpf3 = $tidpf2;
-        $tidpf2 = $tid;
-    } elseif ($stats_pf > $pf3) {
-        $pf5 = $pf4;
-        $pf4 = $pf3;
-        $pf3 = $stats_pf;
-        $name_pf5 = $name_pf4;
-        $name_pf4 = $name_pf3;
-        $name_pf3 = $name;
-        $teamname_pf5 = $teamname_pf4;
-        $teamname_pf4 = $teamname_pf3;
-        $teamname_pf3 = $teamname;
-        $pidpf5 = $pidpf4;
-        $pidpf4 = $pidpf3;
-        $pidpf3 = $pid;
-        $tidpf5 = $tidpf4;
-        $tidpf4 = $tidpf3;
-        $tidpf3 = $tid;
-    } elseif ($stats_pf > $pf4) {
-        $pf5 = $pf4;
-        $pf4 = $stats_pf;
-        $name_pf5 = $name_pf4;
-        $name_pf4 = $name;
-        $teamname_pf5 = $teamname_pf4;
-        $teamname_pf4 = $teamname;
-        $pidpf5 = $pidpf4;
-        $pidpf4 = $pid;
-        $tidpf5 = $tidpf4;
-        $tidpf4 = $tid;
-    } elseif ($stats_pf > $pf5) {
-        $pf5 = $stats_pf;
-        $name_pf5 = $name;
-        $teamname_pf5 = $teamname;
-        $pidpf5 = $pid;
-        $tidpf5 = $tid;
-    }
-
     $i++;
 }
 
@@ -954,36 +516,6 @@ $blk2 = sprintf('%4.1f', $blk2);
 $blk3 = sprintf('%4.1f', $blk3);
 $blk4 = sprintf('%4.1f', $blk4);
 $blk5 = sprintf('%4.1f', $blk5);
-
-$fgm1 = sprintf('%4.1f', $fgm1);
-$fgm2 = sprintf('%4.1f', $fgm2);
-$fgm3 = sprintf('%4.1f', $fgm3);
-$fgm4 = sprintf('%4.1f', $fgm4);
-$fgm5 = sprintf('%4.1f', $fgm5);
-
-$ftm1 = sprintf('%4.1f', $ftm1);
-$ftm2 = sprintf('%4.1f', $ftm2);
-$ftm3 = sprintf('%4.1f', $ftm3);
-$ftm4 = sprintf('%4.1f', $ftm4);
-$ftm5 = sprintf('%4.1f', $ftm5);
-
-$tgm1 = sprintf('%4.1f', $tgm1);
-$tgm2 = sprintf('%4.1f', $tgm2);
-$tgm3 = sprintf('%4.1f', $tgm3);
-$tgm4 = sprintf('%4.1f', $tgm4);
-$tgm5 = sprintf('%4.1f', $tgm5);
-
-$to1 = sprintf('%4.1f', $to1);
-$to2 = sprintf('%4.1f', $to2);
-$to3 = sprintf('%4.1f', $to3);
-$to4 = sprintf('%4.1f', $to4);
-$to5 = sprintf('%4.1f', $to5);
-
-$pf1 = sprintf('%4.1f', $pf1);
-$pf2 = sprintf('%4.1f', $pf2);
-$pf3 = sprintf('%4.1f', $pf3);
-$pf4 = sprintf('%4.1f', $pf4);
-$pf5 = sprintf('%4.1f', $pf5);
 
 $content .= "<center><table border=1 bordercolor=#000066><tr><td><table><tr><td colspan=2><center><img src=\"./images/player/$pid1.jpg\" height=\"90\" width=\"65\"> <img src=\"./images/logo/new$tid1.png\" height=\"75\" width=\"75\"></center></td></tr>
 <tr><td bgcolor=#000066 colspan=2><b><font color=#ffffff>Points</td></tr>

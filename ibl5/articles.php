f863936669426652d24e63e5c2fde3febe835f1a
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-08-15 17:14:01 -0700

    Group clauses together instead of separately

--- ibl5/articles.php
+++ ibl5/articles.php
@@ -6,8 +6,7 @@ $player = $_REQUEST['player'];
 $query = "SELECT *
     FROM nuke_stories
     WHERE (hometext LIKE '%$player%' OR bodytext LIKE '%$player%')
-        AND hometext NOT LIKE '%$player II%'
-        AND bodytext NOT LIKE '%$player II%'
+        AND (hometext NOT LIKE '%$player II%' OR bodytext NOT LIKE '%$player II%')
         ORDER BY time DESC;";
 $result = $db->sql_query($query);
 $num = $db->sql_numrows($result);
@@ -21,8 +20,7 @@ while ($i < $num) {
     $title = $db->sql_result($result, $i, "title");
     $time = $db->sql_result($result, $i, "time");
 
-    echo "
-* <a href=\"modules.php?name=News&file=article&sid=$sid&mode=&order=0&thold=0\">$title</a> ($time)<br>";
+    echo "* <a href=\"modules.php?name=News&file=article&sid=$sid&mode=&order=0&thold=0\">$title</a> ($time)<br>";
 
     $i++;
 }

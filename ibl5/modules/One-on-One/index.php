0fa882dd44ff7fb63f4e8076987b30d9169e1ee0
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-10-28 21:36:16 -0700

    Move Player Search to its own module

--- ibl5/modules/One-on-One/index.php
+++ ibl5/modules/One-on-One/index.php
@@ -28,7 +28,7 @@ oneonone();
 function menu()
 {
     echo "<center><b>
-	<a href=\"modules.php?name=Player&pa=search\">Player Search</a>  |
+	<a href=\"modules.php?name=Player_Search\">Player Search</a>  |
 	<a href=\"modules.php?name=Player&pa=awards\">Awards Search</a> |
 	<a href=\"modules.php?name=One-on-One\">One-On-One Game</a> |
 	<a href=\"modules.php?name=Leaderboards\">Career Leaderboards</a> (All Types)

76509911a348fdb1e802e44eff99d047a54d53fc
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-10-28 23:09:08 -0700

    Move Player Awards into its own module

--- ibl5/classes/UI.php
+++ ibl5/classes/UI.php
@@ -6,7 +6,7 @@ class UI
     {
         echo "<center><b>
             <a href=\"modules.php?name=Player_Search\">Player Search</a>  |
-            <a href=\"modules.php?name=Player&pa=awards\">Awards Search</a> |
+            <a href=\"modules.php?name=Player_Awards\">Awards Search</a> |
             <a href=\"modules.php?name=One-on-One\">One-on-One Game</a> |
             <a href=\"modules.php?name=Leaderboards\">Career Leaderboards</a> (All Types)
         </b><center>

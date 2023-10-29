436dbd068ffdbbbc6166308bbd73a6a081e5f3e5
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-10-28 21:36:16 -0700

    Move Player Search to its own module

--- ibl5/modules/Leaderboards/index.php
+++ ibl5/modules/Leaderboards/index.php
@@ -31,7 +31,7 @@ include "header.php";
 OpenTable();
 
 echo "<center><b>
-<a href=\"modules.php?name=Player&pa=search\">Player Search</a>  |
+<a href=\"modules.php?name=Player_Search\">Player Search</a>  |
 <a href=\"modules.php?name=Player&pa=awards\">Awards Search</a> |
 <a href=\"modules.php?name=One-on-One\">One-on-One Game</a> |
 <a href=\"modules.php?name=Leaderboards\">Career Leaderboards</a> (All Types)

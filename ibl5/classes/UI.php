cfe3bd2c9207330506c5d02b6e6f2489ac2a1ec0
A-Jay Nicolas <ajaynicolas@gmail.com>
2023-10-28 22:51:13 -0700

    Move playerMenu() to create a new UI class

---
+++ ibl5/classes/UI.php
@@ -0,0 +1,15 @@
+<?php
+
+class UI
+{
+    public static function playerMenu()
+    {
+        echo "<center><b>
+            <a href=\"modules.php?name=Player_Search\">Player Search</a>  |
+            <a href=\"modules.php?name=Player&pa=awards\">Awards Search</a> |
+            <a href=\"modules.php?name=One-on-One\">One-on-One Game</a> |
+            <a href=\"modules.php?name=Leaderboards\">Career Leaderboards</a> (All Types)
+        </b><center>
+        <hr>";
+    }
+}
--- ibl5/modules/Leaderboards/index.php
+++ ibl5/modules/Leaderboards/index.php
@@ -29,13 +29,7 @@ $pagetitle = "- Player Archives";
 
 include "header.php";
 OpenTable();
-
-echo "<center><b>
-<a href=\"modules.php?name=Player_Search\">Player Search</a>  |
-<a href=\"modules.php?name=Player&pa=awards\">Awards Search</a> |
-<a href=\"modules.php?name=One-on-One\">One-on-One Game</a> |
-<a href=\"modules.php?name=Leaderboards\">Career Leaderboards</a> (All Types)
-</b><hr>";
+UI::playerMenu();
 
 $boards_type = $_POST['boards_type'];
 $display = $_POST['display'];

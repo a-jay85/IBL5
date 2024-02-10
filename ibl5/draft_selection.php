8b7f0851ed24292b9c7b76dfebc9046204774324
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-02-10 13:20:17 -0800

    Add draft conclusion message; copy tweaks

--- ibl5/draft_selection.php
+++ ibl5/draft_selection.php
@@ -35,20 +35,25 @@ if (($currentDraftSelection == NULL OR $currentDraftSelection == "") AND $player
     $resultUpdateRookieTable = $db->sql_query($queryUpdateRookieTable);
 
     if ($resultUpdateDraftTable AND $resultUpdateRookieTable) {
-        $message = "With pick number $draft_pick in round $draft_round of the $currentSeasonEndingYear IBL Draft, the **" . $teamname . "** select **" . $playerToBeDrafted . "!**";
-        echo "$message<br>
+        $message = "With pick #$draft_pick in round $draft_round of the $currentSeasonEndingYear IBL Draft, the **" . $teamname . "** select **" . $playerToBeDrafted . "!**";
+        echo "$message<p>
         <a href=\"modules.php?name=College_Scouting\">Go back to the Draft module</a>";
     
         $queryTeamOnTheClock = "SELECT team from ibl_draft WHERE player = '' ORDER BY round ASC, pick ASC LIMIT 1";
         $resultTeamOnTheClock = $db->sql_query($queryTeamOnTheClock);
         $teamOnTheClock = $db->sql_result($resultTeamOnTheClock, 0);
 
-        $queryDiscordIDOfTeamOnTheClock = "SELECT discordID from ibl_team_info WHERE team_name = '$teamOnTheClock' LIMIT 1;";
-        $resultDiscordIDOfTeamOnTheClock = $db->sql_query($queryDiscordIDOfTeamOnTheClock);
-        $discordIDOfTeamOnTheClock = $db->sql_result($resultDiscordIDOfTeamOnTheClock, 0);
-
-        $message .= '
-**<@!' . $discordIDOfTeamOnTheClock . '>** is on the clock!';
+        if ($teamOnTheClock != NULL) {
+            $queryDiscordIDOfTeamOnTheClock = "SELECT discordID from ibl_team_info WHERE team_name = '$teamOnTheClock' LIMIT 1;";
+            $resultDiscordIDOfTeamOnTheClock = $db->sql_query($queryDiscordIDOfTeamOnTheClock);
+            $discordIDOfTeamOnTheClock = $db->sql_result($resultDiscordIDOfTeamOnTheClock, 0);
+    
+            $message .= '
+    **<@!' . $discordIDOfTeamOnTheClock . '>** is on the clock!';
+        } else {
+            $message .= "
+    **🏁 __The $currentSeasonEndingYear IBL Draft has officially concluded!__ 🏁**";
+        }
 
         Discord::postToChannel('#draft-picks', $message);
     } else {

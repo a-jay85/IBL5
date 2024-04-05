43dfdd6f8a1b5e6df9d413ca8785b0dbd8b6fc7a
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-04-04 22:16:52 -0700

    Rename ibl_EOY_Votes to ibl_votes_EOY

--- ibl5/eoy_vote_reset.php
+++ ibl5/eoy_vote_reset.php
@@ -2,7 +2,7 @@
 
 require 'mainfile.php';
 
-$query1 = "UPDATE ibl_EOY_Votes SET MVP_1 = NULL, MVP_2 = NULL, MVP_3 = NULL, Six_1 = NULL, Six_2 = NULL, Six_3 = NULL, ROY_1 = NULL, ROY_2 = NULL, ROY_3 = NULL, GM_1 = NULL, GM_2 = NULL, GM_3 = NULL";
+$query1 = "UPDATE ibl_votes_EOY SET MVP_1 = NULL, MVP_2 = NULL, MVP_3 = NULL, Six_1 = NULL, Six_2 = NULL, Six_3 = NULL, ROY_1 = NULL, ROY_2 = NULL, ROY_3 = NULL, GM_1 = NULL, GM_2 = NULL, GM_3 = NULL";
 $result1 = $db->sql_query($query1);
 
 $query2 = "UPDATE ibl_settings SET value = 'Yes' where name = 'EOY Voting'";

fc85e707389533718f908c6986fc4232344e19f5
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-07-21 22:44:31 -0700

    Limit display to 8 teams

--- 2003olympics/blocks/block-AJstandings.php
+++ 2003olympics/blocks/block-AJstandings.php
@@ -27,17 +27,17 @@ $content .= "<center>-to-</center>";
 $content .= "<center><strong>$season->lastSimEndDate</strong></center>";
 $content .= '<tr><td colspan=2><hr></td></tr>';
 
-$queryEasternConference = "SELECT tid, team_name, leagueRecord, confGB, clinchedConference, clinchedDivision, clinchedPlayoffs
+$queryOlympicTeams = "SELECT tid, team_name, leagueRecord, confGB, clinchedConference, clinchedDivision, clinchedPlayoffs
     FROM ibl_standings
-    WHERE conference = 'Eastern'
+    WHERE tid BETWEEN 1 AND 8
     ORDER BY confGB ASC";
-$resultEasternConference = $db->sql_query($queryEasternConference);
-$limitEasternConference = $db->sql_numrows($resultEasternConference);
+$resultOlympicTeams = $db->sql_query($queryOlympicTeams);
+$limitOlympicTeams = $db->sql_numrows($resultOlympicTeams);
 
 $content .= '
     <tr>
         <td colspan=2>
-            <center><font color=#fd004d><b>Eastern Conference</b></font></center>
+            <center><font color=#fd004d><b>' . $season->endingYear . ' Olympic Games</b></font></center>
         </td>
     </tr>
     <tr bgcolor=#006cb3>
@@ -50,70 +50,14 @@ $content .= '
     </tr>';
 
 $i = 0;
-while ($i < $limitEasternConference) {
-    $tid = $db->sql_result($resultEasternConference, $i, 0);
-    $team_name = trim($db->sql_result($resultEasternConference, $i, 1));
-    $leagueRecord = $db->sql_result($resultEasternConference, $i, 2);
-    $confGB = $db->sql_result($resultEasternConference, $i, 3);
-    $clinchedConference = $db->sql_result($resultEasternConference, $i, 4);
-    $clinchedDivision = $db->sql_result($resultEasternConference, $i, 5);
-    $clinchedPlayoffs = $db->sql_result($resultEasternConference, $i, 6);
-    if ($clinchedConference == 1) {
-        $team_name = "<b>Z</b>-" . $team_name;
-    } elseif ($clinchedDivision == 1) {
-        $team_name = "<b>Y</b>-" . $team_name;
-    } elseif ($clinchedPlayoffs == 1) {
-        $team_name = "<b>X</b>-" . $team_name;
-    }
-
-    $content .= '
-        <tr>
-            <td nowrap>
-                <a href="modules.php?name=Team&op=team&tid=' . $tid . '">' . $team_name . '</a> (' . $leagueRecord . ')
-            </td>
-            <td>
-                ' . $confGB . '
-            </td>
-        </tr>';
-    $i++;
-}
-
-$queryWesternConference = "SELECT tid, team_name, leagueRecord, confGB, clinchedConference, clinchedDivision, clinchedPlayoffs
-    FROM ibl_standings
-    WHERE conference = 'Western'
-    ORDER BY confGB ASC";
-$resultWesternConference = $db->sql_query($queryWesternConference);
-$limitWesternConference = $db->sql_numrows($resultWesternConference);
-
-$content .= '
-    <tr>
-        <td colspan=2>
-            <hr>
-        </td>
-    </tr>
-    <tr>
-        <td colspan=2>
-            <center><font color=#fd004d><b>Western Conference</b></font></center>
-        </td>
-    </tr>
-    <tr bgcolor=#006cb3>
-        <td>
-            <center><font color=#ffffff><b>Team (W-L)</b></font></center>
-        </td>
-        <td>
-            <center><font color=#ffffff><b>GB</b></font></center>
-        </td>
-    </tr>';
-
-$i = 0;
-while ($i < $limitWesternConference) {
-    $tid = $db->sql_result($resultWesternConference, $i, 0);
-    $team_name = trim($db->sql_result($resultWesternConference, $i, 1));
-    $leagueRecord = $db->sql_result($resultWesternConference, $i, 2);
-    $confGB = $db->sql_result($resultWesternConference, $i, 3);
-    $clinchedConference = $db->sql_result($resultWesternConference, $i, 4);
-    $clinchedDivision = $db->sql_result($resultWesternConference, $i, 5);
-    $clinchedPlayoffs = $db->sql_result($resultWesternConference, $i, 6);
+while ($i < $limitOlympicTeams) {
+    $tid = $db->sql_result($resultOlympicTeams, $i, 0);
+    $team_name = trim($db->sql_result($resultOlympicTeams, $i, 1));
+    $leagueRecord = $db->sql_result($resultOlympicTeams, $i, 2);
+    $confGB = $db->sql_result($resultOlympicTeams, $i, 3);
+    $clinchedConference = $db->sql_result($resultOlympicTeams, $i, 4);
+    $clinchedDivision = $db->sql_result($resultOlympicTeams, $i, 5);
+    $clinchedPlayoffs = $db->sql_result($resultOlympicTeams, $i, 6);
     if ($clinchedConference == 1) {
         $team_name = "<b>Z</b>-" . $team_name;
     } elseif ($clinchedDivision == 1) {

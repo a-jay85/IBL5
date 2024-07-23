5b27af27dd787d3b9c189a10619e997140ab7695
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-07-22 19:30:23 -0700

    Revert "Delete unused and duplicate files"
    This reverts commit a075151ce515ed9f82b8f7564b2c5e6bacdececa.

---
+++ ibl5/stats_update.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+require 'config.php';
+mysql_connect($dbhost,$dbuname,$dbpass);
+@mysql_select_db($dbname) or die("Unable to select database");
+*/
+
+// Perform Query
+//$db->sql_query("update iblhoops_iblv2forums.iblv2user a, iblhoops_ibl5.ibl_power b, iblhoops_iblv2forums.v4_forum_stats c set a.usertitle= CONCAT(a.usertitle2, " (",b.win,\-\,b.loss,")","<br><br><b>PPG:</b>","&nbsp; &nbsp;", \<a href ="http://iblhoops.net/ibl5/modules.php?name=Player&pa=showpage&pid=\, c.pts_pid,\">\,c.pts_lead,\</a>\, " (", c.pts_num, ")","<br><b>RPG:</b>","&nbsp; &nbsp;", \<a href ="http://iblhoops.net/ibl5/modules.php?name=Player&pa=showpage&pid=\, c.reb_pid,\">\,c.reb_lead,\</a>\, " (", c.reb_num, ")","<br><b>APG:</b>","&nbsp; &nbsp;",\<a href ="http://iblhoops.net/ibl5/modules.php?name=Player&pa=showpage&pid=\, c.ast_pid,\">\,c.ast_lead,\</a>\, " (", c.ast_num, ")<br>") where a.teamname = b.Team and c.teamname = a.teamname");
+
+$test = array(
+            'a' => "Lorem Ipsum",
+            'b' => "ipsum u (",b.win,'-',b.loss,")
+);
+print_r($test);
+
+
+?>

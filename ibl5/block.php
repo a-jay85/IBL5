56b9059c19a86cb506c9c54ca9bce36b9d500881
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-08-20 23:30:20 -0700

    Display total number of offers

--- ibl5/block.php
+++ ibl5/block.php
@@ -13,6 +13,7 @@ echo "<HTML>
 	<HEAD><TITLE>Free Agent Processing</TITLE></HEAD>
 	<BODY>
         <H1>You are viewing <font color=red>Day " . ($val + 1) . "</font> results!</H1>
+        <H2>Total number of offers: $num</H2>
 		<TABLE BORDER=1>
 			<TR>
 				<TD COLSPAN=8>Free Agent Signings</TD>

b121db03de83d33781232c9304661034e1a5dc0c
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-09-30 16:41:26 -0700

    Add Game class

---
+++ ibl5/classes/Game.php
@@ -0,0 +1,27 @@
+<?php
+
+class Game
+{
+    public $date;
+    public $dateObject;
+    public $boxScoreID;
+
+    public $visitorTeamID;
+    public $homeTeamID;
+
+    public $visitorScore;
+    public $homeScore;
+
+    public function __construct($db, $scheduleRow)
+    {
+        $this->date = $scheduleRow['Date'];
+        $this->dateObject = date_create($this->date);
+        $this->boxScoreID = $scheduleRow['BoxID'];
+
+        $this->visitorTeamID = $scheduleRow['Visitor'];
+        $this->homeTeamID = $scheduleRow['Home'];
+
+        $this->visitorScore = $scheduleRow['VScore'];
+        $this->homeScore = $scheduleRow['HScore'];
+    }
+}

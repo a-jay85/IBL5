141a90890f9e99bb9a4f11cedcafcfca2bf33e38
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-01-23 21:59:07 -0800

    Setup team's starters' data

--- ibl5/modules/Next_Sim/index.php
+++ ibl5/modules/Next_Sim/index.php
@@ -2,6 +2,7 @@
 
 global $db, $cookie;
 $sharedFunctions = new Shared($db);
+$season = new Season($db);
 
 if (!defined('MODULE_FILE')) {
     die("You can't access this file directly...");
@@ -13,6 +14,26 @@ $pagetitle = "- $module_name";
 
 $username = $cookie[1];
 $userTeam = Team::initialize($db, $sharedFunctions->getTeamnameFromUsername($username));
+$userStartingPG = Player::withPlayerID($db, $userTeam->getLastSimStarterPlayerIDForPosition('PG') ?? 4040404);
+$userStartingSG = Player::withPlayerID($db, $userTeam->getLastSimStarterPlayerIDForPosition('SG') ?? 4040404);
+$userStartingSF = Player::withPlayerID($db, $userTeam->getLastSimStarterPlayerIDForPosition('SF') ?? 4040404);
+$userStartingPF = Player::withPlayerID($db, $userTeam->getLastSimStarterPlayerIDForPosition('PF') ?? 4040404);
+$userStartingC = Player::withPlayerID($db, $userTeam->getLastSimStarterPlayerIDForPosition('C') ?? 4040404);
+
+$resultUserTeamProjectedGamesNextSim = Schedule\TeamSchedule::getProjectedGamesNextSimResult($db, $userTeam->teamID, $season->lastSimEndDate);
+
+$i = 0;
+foreach ($resultUserTeamProjectedGamesNextSim as $gameRow) {
+    $rows[$i]['game'] = new Game($gameRow);
+    $rows[$i]['opposingTeam'] = Team::initialize($db, $rows[$i]['game']->getOpposingTeamID($userTeam->teamID));
+    $rows[$i]['opposingStartingPG'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('PG') ?? 4040404);
+    $rows[$i]['opposingStartingSG'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('SG') ?? 4040404);
+    $rows[$i]['opposingStartingSF'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('SF') ?? 4040404);
+    $rows[$i]['opposingStartingPF'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('PF') ?? 4040404);
+    $rows[$i]['opposingStartingC'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('C') ?? 4040404);
+
+    $i++;
+}
 
 ?>
 

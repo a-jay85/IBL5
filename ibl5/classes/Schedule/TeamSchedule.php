e2e49559e8cf0ebc30854d4727c70b00e8c61c32
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-01-23 21:37:13 -0800

    Add getProjectedGamesNextSimResult()

--- ibl5/classes/Schedule/TeamSchedule.php
+++ ibl5/classes/Schedule/TeamSchedule.php
@@ -11,6 +11,16 @@ class TeamSchedule extends Schedule
             WHERE Visitor = $teamID
                OR Home = $teamID
             ORDER BY Date ASC;";
-        return $db->sql_query($query);;
+        return $db->sql_query($query);
+    }
+
+    public static function getProjectedGamesNextSimResult($db, $teamID, $lastSimEndDate)
+    {
+        $query = "SELECT *
+            FROM `ibl_schedule`
+            WHERE (Visitor = $teamID OR Home = $teamID)
+              AND Date BETWEEN '$lastSimEndDate' AND ADDDATE('$lastSimEndDate', " . \Sim::LENGTH_IN_DAYS . ")
+            ORDER BY Date ASC;";
+        return $db->sql_query($query);
     }
 }

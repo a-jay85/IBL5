9e1eb09751645090b1f4efa9dc7021c7451ff3ee
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-02-11 22:29:50 -0800

    Use constant for waivers ordinal cut-off number

--- ibl5/classes/JSB.php
+++ ibl5/classes/JSB.php
@@ -4,4 +4,5 @@ class JSB
 {
     const PLAYER_POSITIONS = ['PG', 'SG', 'SF', 'PF', 'C'];
     const PLAYOFF_MONTH = 22;
+    const WAIVERS_ORDINAL = 960; // Any player with an ordinal > 960 is considered by JSB to be on waivers
 }

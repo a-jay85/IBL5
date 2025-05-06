a21d6febd56ff6e3e63d5c613457e11b4693042b
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-05-06 13:10:46 -0700

    Display sim day, move date to hover text

--- ibl5/modules/Next_Sim/index.php
+++ ibl5/modules/Next_Sim/index.php
@@ -21,10 +21,13 @@ $userStartingPF = Player::withPlayerID($db, $userTeam->getCurrentlySetStarterPla
 $userStartingC = Player::withPlayerID($db, $userTeam->getCurrentlySetStarterPlayerIDForPosition('C') ?? 4040404);
 
 $resultUserTeamProjectedGamesNextSim = Schedule\TeamSchedule::getProjectedGamesNextSimResult($db, $userTeam->teamID, $season->lastSimEndDate);
+$lastSimEndDateObject = new DateTime($season->lastSimEndDate);
 
 $i = 0;
 foreach ($resultUserTeamProjectedGamesNextSim as $gameRow) {
     $rows[$i]['game'] = new Game($gameRow);
+    $rows[$i]['date'] = new DateTime($rows[$i]['game']->date);
+    $rows[$i]['day'] = $rows[$i]['date']->diff($lastSimEndDateObject)->format("%a");
     $rows[$i]['opposingTeam'] = Team::initialize($db, $rows[$i]['game']->getOpposingTeamID($userTeam->teamID));
     $rows[$i]['opposingStartingPG'] = Player::withPlayerID($db, $rows[$i]['opposingTeam']->getLastSimStarterPlayerIDForPosition('PG') ?? 4040404);
     $rows[$i]['userStartingPG'] = $userStartingPG ?? 4040404;
@@ -60,7 +63,7 @@ foreach ($resultUserTeamProjectedGamesNextSim as $gameRow) {
                         <table align=center>
                             <tr>
                                 <td style="text-align: right;" width=150>
-                                    <h2><?= $rows[$i]['game']->date . ": " . $rows[$i]['game']->getUserTeamLocationPrefix($userTeam->teamID) ?></h2>
+                                    <h2 title="<?= $rows[$i]['game']->date ?>"><?= "Day " . $rows[$i]['day'] . " " . $rows[$i]['game']->getUserTeamLocationPrefix($userTeam->teamID) ?></h2>
                                 </td>
                                 <td style="text-align: center; padding-left: 4px; padding-right: 4px">
                                     <a href="modules.php?name=Team&op=team&tid=<?= $rows[$i]['opposingTeam']->teamID ?>">

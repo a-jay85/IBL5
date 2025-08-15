664d5ecebfd118cd6d1c772ec4b7b02204d4dd04
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-08-15 13:22:49 -0700

    Remove PHP-NUKE comment block

--- ibl5/modules/One-on-One/index.php
+++ ibl5/modules/One-on-One/index.php
@@ -1,17 +1,5 @@
 <?php
 
-/************************************************************************/
-/* PHP-NUKE: Web Portal System                                          */
-/* ===========================                                          */
-/*                                                                      */
-/* Copyright (c) 2007 by Francisco Burzi                                */
-/* http://phpnuke.org                                                   */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
-/************************************************************************/
-
 if (!defined('MODULE_FILE')) {
     die("You can't access this file directly...");
 }

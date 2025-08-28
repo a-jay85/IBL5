8e76fedab2e7f95350294b217833df73142c59d8
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-08-27 23:48:36 -0700

    Remove PHP-NUKE comment blocks

--- ibl5/modules/Free_Agency/index.php
+++ ibl5/modules/Free_Agency/index.php
@@ -1,20 +1,6 @@
 <?php
 
 /************************************************************************/
-/* PHP-NUKE: Web Portal System                                          */
-/* ===========================                                          */
-/*                                                                      */
-/* Copyright (c) 2002 by Francisco Burzi                                */
-/* http://phpnuke.org                                                   */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
-/************************************************************************/
-/*         Additional security & Abstraction layer conversion           */
-/*                           2003 chatserv                              */
-/*      http://www.nukefixes.com -- http://www.nukeresources.com        */
-/************************************************************************/
 /*                     IBL Free Agency Module                           */
 /*               (c) July 22, 2005 by Spencer Cooley                    */
 /************************************************************************/

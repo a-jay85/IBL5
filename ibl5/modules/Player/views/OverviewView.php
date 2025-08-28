e535ce4caa50dc5e3d83e789a96485837ae22f50
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-08-28 14:21:44 -0700

    Cleanup and close table tags

--- ibl5/modules/Player/views/OverviewView.php
+++ ibl5/modules/Player/views/OverviewView.php
@@ -4,49 +4,38 @@ require_once __DIR__ . '/BaseView.php';
 
 class OverviewView extends BaseView {
     public function render() {
-        // NOTE ALL-STAR WEEKEND APPEARANCES
-
-        echo "<tr>
-            <td colspan=3>
-                <table align=left cellspacing=1 cellpadding=0 border=1>
-                    <th colspan=2><center>All-Star Activity</center></th>
-        </tr>";
-
         $allstarquery = $this->db->sql_query("SELECT * FROM ibl_awards WHERE name='" . $this->player->name . "' AND Award LIKE '%Conference All-Star'");
         $asg = $this->db->sql_numrows($allstarquery);
-        echo "<tr>
-            <td><b>All Star Games:</b></td>
-            <td>$asg</td>
-        </tr>";
 
         $allstarquery2 = $this->db->sql_query("SELECT * FROM ibl_awards WHERE name='" . $this->player->name . "' AND Award LIKE 'Three-Point Contest%'");
         $threepointcontests = $this->db->sql_numrows($allstarquery2);
 
-        echo "<tr>
-            <td><b>Three-Point<br>Contests:</b></td>
-            <td>$threepointcontests</td>
-        </tr>";
-
         $allstarquery3 = $this->db->sql_query("SELECT * FROM ibl_awards WHERE name='" . $this->player->name . "' AND Award LIKE 'Slam Dunk Competition%'");
         $dunkcontests = $this->db->sql_numrows($allstarquery3);
-
-        echo "<tr>
-            <td><b>Slam Dunk<br>Competitions:</b></td>
-            <td>$dunkcontests</td>
-        </tr>";
-
+        
         $allstarquery4 = $this->db->sql_query("SELECT * FROM ibl_awards WHERE name='" . $this->player->name . "' AND Award LIKE 'Rookie-Sophomore Challenge'");
         $rooksoph = $this->db->sql_numrows($allstarquery4);
 
-        echo "<tr>
-            <td><b>Rookie-Sophomore<br>Challenges:</b></td>
-            <td>$rooksoph</td>
-        </tr>
-        </table>";
-
-        // END ALL-STAR WEEKEND ACTIVITY SCRIPT
-
-        echo "<center>
+        echo "<table align=left cellspacing=1 cellpadding=0 border=1>
+            <th colspan=2><center>All-Star Activity</center></th>
+            <tr>
+                <td><b>All Star Games:</b></td>
+                <td>$asg</td>
+            </tr>
+            <tr>
+                <td><b>Three-Point<br>Contests:</b></td>
+                <td>$threepointcontests</td>
+            </tr>
+            <tr>
+                <td><b>Slam Dunk<br>Competitions:</b></td>
+                <td>$dunkcontests</td>
+            </tr>
+            <tr>
+                <td><b>Rookie-Sophomore<br>Challenges:</b></td>
+                <td>$rooksoph</td>
+            </tr>
+        </table>
+        <center>
         <table>
             <tr align=center>
                 <td><b>Talent</b></td>
@@ -79,7 +68,6 @@ class OverviewView extends BaseView {
                 <td>" . $this->player->freeAgencyTradition . "</td>
             </tr>
         </table>
-        </center>
-        </td></tr></table>";
+        </center>";
     }
 }

e4ed59bee516201e390e2d032fd8b15609120c22
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-08-28 17:06:07 -0700

    Move Game Log into Overview

--- ibl5/modules/Player/views/BaseView.php
+++ ibl5/modules/Player/views/BaseView.php
@@ -4,8 +4,8 @@ abstract class BaseView {
     protected $db;
     protected $player;
     protected $playerStats;
-    
-    public function __construct($db, $player, $playerStats) {
+
+    public function __construct($db, Player $player, PlayerStats $playerStats) {
         $this->db = $db;
         $this->player = $player;
         $this->playerStats = $playerStats;
--- ibl5/modules/Player/views/OverviewView.php
+++ ibl5/modules/Player/views/OverviewView.php
@@ -3,6 +3,15 @@
 require_once __DIR__ . '/BaseView.php';
 
 class OverviewView extends BaseView {
+    private $season;
+    private $sharedFunctions;
+
+    public function __construct($db, Player $player, PlayerStats $playerStats, Season $season, Shared $sharedFunctions) {
+        parent::__construct($db, $player, $playerStats);
+        $this->season = $season;
+        $this->sharedFunctions = $sharedFunctions;
+    }
+
     public function render() {
         $allstarquery = $this->db->sql_query("SELECT * FROM ibl_awards WHERE name='" . $this->player->name . "' AND Award LIKE '%Conference All-Star'");
         $asg = $this->db->sql_numrows($allstarquery);
@@ -69,5 +78,80 @@ class OverviewView extends BaseView {
             </tr>
         </table>
         </center>";
+
+        if ($this->season->phase == "Preseason") {
+            $query = "SELECT * FROM ibl_box_scores WHERE Date BETWEEN '" . $this->season->beginningYear . "-" . Season::IBL_PRESEASON_MONTH . "-01' AND '" . $this->season->endingYear . "-07-01' AND pid = " . $this->player->playerID . " ORDER BY Date ASC";
+        } elseif ($this->season->phase == "HEAT") {
+            $query = "SELECT * FROM ibl_box_scores WHERE Date BETWEEN '" . $this->season->beginningYear . "-" . Season::IBL_HEAT_MONTH . "-01' AND '" . $this->season->endingYear . "-07-01' AND pid = " . $this->player->playerID . " ORDER BY Date ASC";
+        } else {
+            $query = "SELECT * FROM ibl_box_scores WHERE Date BETWEEN '" . $this->season->beginningYear . "-" . Season::IBL_REGULAR_SEASON_STARTING_MONTH . "-01' AND '" . $this->season->endingYear . "-07-01' AND pid = " . $this->player->playerID . " ORDER BY Date ASC";
+        }
+        $result = $this->db->sql_query($query);
+
+        echo "<p>
+            <H1><center>GAME LOG</center></H1>
+            <p>
+            <table class=\"sortable\" width=\"100%\">
+                <tr>
+                    <th>Date</th>
+                    <th>Away</th>
+                    <th>Home</th>
+                    <th>MIN</th>
+                    <th>PTS</th>
+                    <th>FGM</th>
+                    <th>FGA</th>
+                    <th>FG%</th>
+                    <th>FTM</th>
+                    <th>FTA</th>
+                    <th>FT%</th>
+                    <th>3GM</th>
+                    <th>3GA</th>
+                    <th>3G%</th>
+                    <th>ORB</th>
+                    <th>DRB</th>
+                    <th>REB</th>
+                    <th>AST</th>
+                    <th>STL</th>
+                    <th>TO</th>
+                    <th>BLK</th>
+                    <th>PF</th>
+                </tr>";
+
+        echo "<style>
+            td {}
+            .gamelog {text-align: center;}
+        </style>";
+
+        while ($row = $this->db->sql_fetch_assoc($result)) {
+            $fieldGoalPercentage = ($row['gameFGA'] + $row['game3GA']) ? number_format(($row['gameFGM'] + $row['game3GM']) / ($row['gameFGA'] + $row['game3GA']), 3, '.', '') : "0.000";
+            $freeThrowPercentage = ($row['gameFTA']) ? number_format($row['gameFTM'] / $row['gameFTA'], 3, '.', '') : "0.000";
+            $threePointPercentage = ($row['game3GA']) ? number_format($row['game3GM'] / $row['game3GA'], 3, '.', '') : "0.000";
+
+            echo "<tr>
+                <td class=\"gamelog\">" . $row['Date'] . "</td>
+                <td class=\"gamelog\">" . $this->sharedFunctions->getTeamnameFromTid($row['homeTID']) . "</td>
+                <td class=\"gamelog\">" . $this->sharedFunctions->getTeamnameFromTid($row['visitorTID']) . "</td>
+                <td class=\"gamelog\">" . $row['gameMIN'] . "</td>
+                <td class=\"gamelog\">" . ((2 * $row['gameFGM']) + (3 * $row['game3GM']) + $row['gameFTM']) . "</td>
+                <td class=\"gamelog\">" . ($row['gameFGM'] + $row['game3GM']) . "</td>
+                <td class=\"gamelog\">" . ($row['gameFGA'] + $row['game3GA']) . "</td>
+                <td class=\"gamelog\">" . $fieldGoalPercentage . "</td>
+                <td class=\"gamelog\">" . $row['gameFTM'] . "</td>
+                <td class=\"gamelog\">" . $row['gameFTA'] . "</td>
+                <td class=\"gamelog\">" . $freeThrowPercentage . "</td>
+                <td class=\"gamelog\">" . $row['game3GM'] . "</td>
+                <td class=\"gamelog\">" . $row['game3GA'] . "</td>
+                <td class=\"gamelog\">" . $threePointPercentage . "</td>
+                <td class=\"gamelog\">" . $row['gameORB'] . "</td>
+                <td class=\"gamelog\">" . $row['gameDRB'] . "</td>
+                <td class=\"gamelog\">" . ($row['gameORB'] + $row['gameDRB']) . "</td>
+                <td class=\"gamelog\">" . $row['gameAST'] . "</td>
+                <td class=\"gamelog\">" . $row['gameSTL'] . "</td>
+                <td class=\"gamelog\">" . $row['gameTOV'] . "</td>
+                <td class=\"gamelog\">" . $row['gameBLK'] . "</td>
+                <td class=\"gamelog\">" . $row['gamePF'] . "</td>
+            </tr>";
+        }
+        echo "</table>";
     }
 }

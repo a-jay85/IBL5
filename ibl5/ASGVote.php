8fa04471f1f139320a0ce5c2522af019117576d8
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-09-29 11:33:10 -0700

    Refactor to make error checks more DRY

--- ibl5/ASGVote.php
+++ ibl5/ASGVote.php
@@ -46,88 +46,71 @@ echo "
         Western Backcourt Player: $WCB4<br>
         <br>";
 
+$positions = [
+    'WCF' => 'Western Frontcourt',
+    'WCB' => 'Western Backcourt',
+    'ECF' => 'Eastern Frontcourt',
+    'ECB' => 'Eastern Backcourt'
+];
+
 // VOTING FOR OWN PLAYERS
-echo "<font color=red>";
-if (strpos($WCF1, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $WCF1).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCF2, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $WCF2).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCF3, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $WCF3).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCF4, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $WCF4).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCB1, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $WCB1).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCB2, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $WCB2).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCB3, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $WCB3).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($WCB4, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $WCB4).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECF1, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $ECF1).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECF2, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $ECF2).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECF3, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $ECF3).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECF4, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Frontcourt: $ECF4).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECB1, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $ECB1).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECB2, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $ECB2).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECB3, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $ECB3).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
-} else if (strpos($ECB4, $Team_Name) !== false) {
-    echo "Sorry, you cannot vote for your own player (Backcourt: $ECB4).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
+foreach ($positions as $prefix => $label) {
+    for ($i = 1; $i <= 4; $i++) {
+        $varName = $prefix . $i;
+        if (strpos($$varName, $Team_Name) !== false) {
+            $court = (strpos($prefix, 'F') !== false) ? 'Frontcourt' : 'Backcourt';
+            echo "Sorry, you cannot vote for your own player ($court: {$$varName}).<p>Please go back, unselect that player, select a different player not on your team, and try again.<br>";
+            Nuke\Footer::footer();
+            exit;
+        }
+    }
 }
 
 // MISSING VOTES
-else if ($ECF1 == "" OR $ECF2 == "" OR $ECF3 == "" OR $ECF4 == "") {
-    echo "Sorry, you selected less than FOUR Eastern Conference Frontcourt Players.<p>Please go back, select FOUR players, and try again.<br>";
-} else if ($ECB1 == "" OR $ECB2 == "" OR $ECB3 == "" OR $ECB4 == "") {
-    echo "Sorry, you selected less than FOUR Eastern Conference Backcourt Players.<p>Please go back, select FOUR players, and try again.<br>";
-} else if ($WCF1 == "" OR $WCF2 == "" OR $WCF3 == "" OR $WCF4 == "") {
-    echo "Sorry, you selected less than FOUR Western Conference Frontcourt Players.<p>Please go back, select FOUR players, and try again.<br>";
-} else if ($WCB1 == "" OR $WCB2 == "" OR $WCB3 == "" OR $WCB4 == "") {
-    echo "Sorry, you selected less than FOUR Western Conference Backcourt Players.<p>Please go back, select FOUR players, and try again.<br>";
+foreach ($positions as $prefix => $label) {
+    for ($i = 1; $i <= 4; $i++) {
+        $varName = $prefix . $i;
+        if (empty($$varName)) {
+            echo "Sorry, you selected less than FOUR $label Players.<p>Please go back, select FOUR players, and try again.<br>";
+            Nuke\Footer::footer();
+            exit;
+        }
+    }
 }
 
 // TOO MANY VOTES
-else if (count($_POST['ECF']) > 4) {
-    echo "Oops, you've selected more than four Eastern Conference Frontcourt Players.<p>Please go back, select FOUR players, and try again.";
-} else if (count($_POST['ECB']) > 4) {
-    echo "Oops, you've selected more than four Eastern Conference Backcourt Players.<p>Please go back, select FOUR players, and try again.";
-} else if (count($_POST['WCF']) > 4) {
-    echo "Oops, you've selected more than four Western Conference Frontcourt Players.<p>Please go back, select FOUR players, and try again.";
-} else if (count($_POST['WCB']) > 4) {
-    echo "Oops, you've selected more than four Western Conference Backcourt Players.<p>Please go back, select FOUR players, and try again.";
-} else {
-    $queryUpdateVotes = "UPDATE ibl_votes_ASG 
-    SET East_F1 = '$ECF1',
-        East_F2 = '$ECF2',
-        East_F3 = '$ECF3',
-        East_F4 = '$ECF4',
-        East_B1 = '$ECB1',
-        East_B2 = '$ECB2',
-        East_B3 = '$ECB3',
-        East_B4 = '$ECB4',
-        West_F1 = '$WCF1',
-        West_F2 = '$WCF2',
-        West_F3 = '$WCF3',
-        West_F4 = '$WCF4',
-        West_B1 = '$WCB1',
-        West_B2 = '$WCB2',
-        West_B3 = '$WCB3',
-        West_B4 = '$WCB4'
-    WHERE team_name = '$Team_Name'";
-
-    if ($db->sql_query($queryUpdateVotes)) {
-        echo "</font><b>Thank you for voting - the $Team_Name vote has been recorded!</b><p>";
-
-        $queryUpdateASGVoteSubmissionTime = "UPDATE ibl_team_history SET asg_vote = NOW() + INTERVAL 2 HOUR WHERE team_name = '$Team_Name'";
-        $resultUpdateASGVoteSubmissionTime = $db->sql_query($queryUpdateASGVoteSubmissionTime);
+foreach ($positions as $prefix => $label) {
+    if (isset($_POST[$prefix]) && count($_POST[$prefix]) > 4) {
+        echo "Oops, you've selected more than four $label Players.<p>Please go back, select FOUR players, and try again.";
+        Nuke\Footer::footer();
+        exit;
     }
+}
+
+$queryUpdateVotes = "UPDATE ibl_votes_ASG 
+SET East_F1 = '$ECF1',
+    East_F2 = '$ECF2',
+    East_F3 = '$ECF3',
+    East_F4 = '$ECF4',
+    East_B1 = '$ECB1',
+    East_B2 = '$ECB2',
+    East_B3 = '$ECB3',
+    East_B4 = '$ECB4',
+    West_F1 = '$WCF1',
+    West_F2 = '$WCF2',
+    West_F3 = '$WCF3',
+    West_F4 = '$WCF4',
+    West_B1 = '$WCB1',
+    West_B2 = '$WCB2',
+    West_B3 = '$WCB3',
+    West_B4 = '$WCB4'
+WHERE team_name = '$Team_Name'";
+
+if ($db->sql_query($queryUpdateVotes)) {
+    echo "</font><b>Thank you for voting - the $Team_Name vote has been recorded!</b><p>";
+
+    $queryUpdateASGVoteSubmissionTime = "UPDATE ibl_team_history SET asg_vote = NOW() + INTERVAL 2 HOUR WHERE team_name = '$Team_Name'";
+    $resultUpdateASGVoteSubmissionTime = $db->sql_query($queryUpdateASGVoteSubmissionTime);
 }
 
 Nuke\Footer::footer();

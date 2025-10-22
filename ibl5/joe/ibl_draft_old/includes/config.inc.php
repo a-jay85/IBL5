375d00945cef17b26768737f53fa43e50fa5eb05
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-10-21 20:08:35 -0700

    chore: delete deprecated draft module

--- ibl5/joe/ibl_draft_old/includes/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?
-$user = "iblhoops_chibul";
-$password = "whereWTFhappens19!";
-$database = "iblhoops_draft";
-$host = "localhost";
-mysql_connect($host, $user, $password) or die("I cannot connect to the database server because: " . mysql_error());
-mysql_select_db($database) or die("I cannot connect to the database because: " . mysql_error());
-define(kAdminUser, '33');

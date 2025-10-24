61a69e1898049af0c13e4f4ddf65da713d288891
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-10-24 11:13:01 -0700

    refactor: delete script that restored PHPNuke's default theme

--- ibl5/restore.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/ibl5/mainfile.php';
-$theme = "DeepBlue";
-$sql = "UPDATE " . $prefix . "_config SET Default_Theme = '" . $theme . "'";
-$result = $db->sql_query($sql);
-echo "Thanks for using this fix! Your theme has been reset to " . $theme . "";

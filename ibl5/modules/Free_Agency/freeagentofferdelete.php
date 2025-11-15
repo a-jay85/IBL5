e78539637dd3bf785ef5e7f464089073188e80a7
A-Jay Nicolas <ajaynicolas@gmail.com>
2025-11-14 21:18:17 -0800

    Escape user input in free agent offer and deletion processes to prevent SQL injection

--- ibl5/modules/Free_Agency/freeagentofferdelete.php
+++ ibl5/modules/Free_Agency/freeagentofferdelete.php
@@ -7,9 +7,15 @@ echo "<HTML><HEAD><TITLE>Free Agency Offer Deletion</TITLE></HEAD><BODY>";
 $teamname = $_POST['teamname'];
 $playername = $_POST['playername'];
 
+$databaseService = new \Services\DatabaseService();
+
+// Escape user input for SQL
+$escaped_playername = $databaseService->escapeString($db, $playername);
+$escaped_teamname = $databaseService->escapeString($db, $teamname);
+
 // ==== ENTER OFFER INTO DATABASE ====
 
-$querychunk = "DELETE FROM `ibl_fa_offers` WHERE `name` = '$playername' AND `team` = '$teamname'";
+$querychunk = "DELETE FROM `ibl_fa_offers` WHERE `name` = '$escaped_playername' AND `team` = '$escaped_teamname'";
 $resultchunk = $db->sql_query($querychunk);
 
 echo "Your offers have been deleted.  This should show up immediately.  Please <a href=\"/ibl5/modules.php?name=Free_Agency\">click here to return to the Free Agency main page</a> (your offer should now be gone).</br>";

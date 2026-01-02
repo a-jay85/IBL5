e216bf2820ad3151672ff45359ac78397fa098ee
A-Jay Nicolas <ajaynicolas@gmail.com>
2026-01-01 17:09:24 -0800

    feat: add LeagueContext support to TradingRepository for multi-league functionality

--- ibl5/classes/Trading/TradingRepository.php
+++ ibl5/classes/Trading/TradingRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Trading;
 
 use BaseMysqliRepository;
+use League\LeagueContext;
 use Trading\Contracts\TradingRepositoryInterface;
 
 /**
@@ -18,18 +19,22 @@ use Trading\Contracts\TradingRepositoryInterface;
  */
 class TradingRepository extends BaseMysqliRepository implements TradingRepositoryInterface
 {
+    private ?LeagueContext $leagueContext;
+
     /**
      * Constructor - inherits from BaseMysqliRepository
      * 
      * @param object $db Active mysqli connection (or duck-typed mock during migration)
+     * @param LeagueContext|null $leagueContext Optional league context for multi-league support
      * @throws \RuntimeException If connection is invalid (error code 1002)
      * 
      * TEMPORARY: Accepts duck-typed objects during mysqli migration for testing.
      * Will be strictly \mysqli once migration completes.
      */
-    public function __construct(object $db)
+    public function __construct(object $db, ?LeagueContext $leagueContext = null)
     {
         parent::__construct($db);
+        $this->leagueContext = $leagueContext;
     }
 
     /**
@@ -49,8 +54,9 @@ class TradingRepository extends BaseMysqliRepository implements TradingRepositor
      */
     public function getAllTeams(): array
     {
+        $teamTable = $this->leagueContext ? $this->leagueContext->getTableName('ibl_team_info') : 'ibl_team_info';
         return $this->fetchAll(
-            "SELECT team_name FROM ibl_team_info ORDER BY team_name"
+            "SELECT team_name FROM {$teamTable} ORDER BY team_name"
         );
     }
 
@@ -452,8 +458,9 @@ class TradingRepository extends BaseMysqliRepository implements TradingRepositor
      */
     public function getAllTeamsWithCity(): array
     {
+        $teamTable = $this->leagueContext ? $this->leagueContext->getTableName('ibl_team_info') : 'ibl_team_info';
         return $this->fetchAll(
-            "SELECT team_name, team_city FROM ibl_team_info ORDER BY team_city ASC"
+            "SELECT team_name, team_city FROM {$teamTable} ORDER BY team_city ASC"
         );
     }
 }

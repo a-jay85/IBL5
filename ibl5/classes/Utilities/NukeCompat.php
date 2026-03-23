44e1363564d0a279909eecb7cff2579dc3406e85
A-Jay Nicolas <ajaynicolas@gmail.com>
2026-03-23 13:28:31 -0700

    fix: PHPStan strict-mode issues in formatTimestamp
    - Use is_string() guard instead of (string) cast on $time
    - Use is_string() check for _DATESTRING constant (mixed type)

--- ibl5/classes/Utilities/NukeCompat.php
+++ ibl5/classes/Utilities/NukeCompat.php
@@ -71,13 +71,13 @@ class NukeCompat
     public function formatTimestamp(int|string $timestamp): string
     {
         $time = $timestamp;
-        if (!is_numeric($time)) {
-            if (preg_match('/(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})/', (string) $time, $dtParts) === 1) {
+        if (is_string($time) && !is_numeric($time)) {
+            if (preg_match('/(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})/', $time, $dtParts) === 1) {
                 $time = gmmktime((int) $dtParts[4], (int) $dtParts[5], (int) $dtParts[6], (int) $dtParts[2], (int) $dtParts[3], (int) $dtParts[1]);
             }
         }
         $unixTime = (int) $time - (int) date("Z");
-        $format = defined('_DATESTRING') ? (string) _DATESTRING : 'l, F d, Y @ H:i';
+        $format = defined('_DATESTRING') && is_string(_DATESTRING) ? _DATESTRING : 'l, F d, Y @ H:i';
         return ucfirst(date($format, $unixTime));
     }
 

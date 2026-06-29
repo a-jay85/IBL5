1d114233b4cb27b79e4c41c2861764071ff61976
A-Jay Nicolas <ajaynicolas@gmail.com>
2026-06-28 19:15:16 -0700

    fix: resolve Clock/ClockInterface rename-conflict markers (keep namespace Clock)

--- ibl5/classes/Clock/ClockInterface.php
+++ ibl5/classes/Clock/ClockInterface.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<<< HEAD:ibl5/classes/Clock/ClockInterface.php
 namespace Clock;
-========
-namespace Api\Contracts;
->>>>>>>> 030b8d1d6 (refactor: Api/ interface coverage + Contracts flatten (findings 2.31, 2.32)):ibl5/classes/Api/Contracts/ClockInterface.php
 
 interface ClockInterface
 {

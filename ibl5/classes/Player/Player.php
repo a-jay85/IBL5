ada9ce54cc4343bca8b94b45eebe90ed6b65772e
A-Jay Nicolas <ajaynicolas@gmail.com>
2026-09-04 13:32:29 -0700

    refactor: extract 20 contract getters into PlayerContractGetters trait (Phase 6)

--- ibl5/classes/Player/Player.php
+++ ibl5/classes/Player/Player.php
@@ -23,7 +23,7 @@ use Season\Season;
  */
 class Player implements PlayerInterface
 {
-    use PlayerIdentityGetters, PlayerRatingsGetters;
+    use PlayerIdentityGetters, PlayerContractGetters, PlayerRatingsGetters;
 
     /** @var \mysqli Database connection */
     protected \mysqli $db;
@@ -126,127 +126,7 @@ class Player implements PlayerInterface
         return $this->playerData;
     }
 
-    // --- Typed getters delegating to PlayerData ---
-
-    /** @see PlayerInterface::getTeamid() */
-    public function getTeamid(): ?int
-    {
-        return $this->playerData?->teamid;
-    }
-
-    /** @see PlayerInterface::getTeamName() */
-    public function getTeamName(): ?string
-    {
-        return $this->playerData?->teamName;
-    }
-
-    /** @see PlayerInterface::getTeamColor1() */
-    public function getTeamColor1(): ?string
-    {
-        return $this->playerData?->teamColor1;
-    }
-
-    /** @see PlayerInterface::getTeamColor2() */
-    public function getTeamColor2(): ?string
-    {
-        return $this->playerData?->teamColor2;
-    }
-
-    /** @see PlayerInterface::getFreeAgencyLoyalty() */
-    public function getFreeAgencyLoyalty(): ?int
-    {
-        return $this->playerData?->freeAgencyLoyalty;
-    }
-
-    /** @see PlayerInterface::getFreeAgencyPlayingTime() */
-    public function getFreeAgencyPlayingTime(): ?int
-    {
-        return $this->playerData?->freeAgencyPlayingTime;
-    }
-
-    /** @see PlayerInterface::getFreeAgencyPlayForWinner() */
-    public function getFreeAgencyPlayForWinner(): ?int
-    {
-        return $this->playerData?->freeAgencyPlayForWinner;
-    }
-
-    /** @see PlayerInterface::getFreeAgencyTradition() */
-    public function getFreeAgencyTradition(): ?int
-    {
-        return $this->playerData?->freeAgencyTradition;
-    }
-
-    /** @see PlayerInterface::getFreeAgencySecurity() */
-    public function getFreeAgencySecurity(): ?int
-    {
-        return $this->playerData?->freeAgencySecurity;
-    }
-
-    /** @see PlayerInterface::getBirdYears() */
-    public function getBirdYears(): ?int
-    {
-        return $this->playerData?->birdYears;
-    }
-
-    /** @see PlayerInterface::getContractCurrentYear() */
-    public function getContractCurrentYear(): ?int
-    {
-        return $this->playerData?->contractCurrentYear;
-    }
-
-    /** @see PlayerInterface::getContractTotalYears() */
-    public function getContractTotalYears(): ?int
-    {
-        return $this->playerData?->contractTotalYears;
-    }
-
-    /** @see PlayerInterface::getContractYear1Salary() */
-    public function getContractYear1Salary(): ?int
-    {
-        return $this->playerData?->contractYear1Salary;
-    }
-
-    /** @see PlayerInterface::getContractYear2Salary() */
-    public function getContractYear2Salary(): ?int
-    {
-        return $this->playerData?->contractYear2Salary;
-    }
-
-    /** @see PlayerInterface::getContractYear3Salary() */
-    public function getContractYear3Salary(): ?int
-    {
-        return $this->playerData?->contractYear3Salary;
-    }
-
-    /** @see PlayerInterface::getContractYear4Salary() */
-    public function getContractYear4Salary(): ?int
-    {
-        return $this->playerData?->contractYear4Salary;
-    }
-
-    /** @see PlayerInterface::getContractYear5Salary() */
-    public function getContractYear5Salary(): ?int
-    {
-        return $this->playerData?->contractYear5Salary;
-    }
-
-    /** @see PlayerInterface::getContractYear6Salary() */
-    public function getContractYear6Salary(): ?int
-    {
-        return $this->playerData?->contractYear6Salary;
-    }
-
-    /** @see PlayerInterface::getSalaryJSB() */
-    public function getSalaryJSB(): ?int
-    {
-        return $this->playerData?->salaryJSB;
-    }
-
-    /** @see PlayerInterface::getTimeDroppedOnWaivers() */
-    public function getTimeDroppedOnWaivers(): ?int
-    {
-        return $this->playerData?->timeDroppedOnWaivers;
-    }
+    // --- Collaborator-backed methods; pure field getters live in the three getter traits ---
 
     /** @see PlayerInterface::getDecoratedName() */
     public function getDecoratedName(): ?string
